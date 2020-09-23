38f6230459978bd973f19615791b750680b5fc54
Kamil Rosiak <kamilrosiak@hotmail.com>
2020-09-23 16:22:50 +0200

    - plugin that contains a universal data structure for the comparison of generic artifacts

---
+++ de.tu_bs.cs.isf.e4cf.compare.data_structures/src/de/tu_bs/cs/isf/e4cf/compare/data_structures/date_structure/impl/NodeImpl.java
@@ -0,0 +1,14 @@
+package de.tu_bs.cs.isf.e4cf.compare.data_structures.date_structure.impl;
+
+import java.util.Map;
+
+import de.tu_bs.cs.isf.e4cf.compare.data_structures.date_structure.AbstractNode;
+import de.tu_bs.cs.isf.e4cf.compare.data_structures.date_structure.util.Values;
+
+public class NodeImpl extends AbstractNode {
+
+
+
+
+	
+}
