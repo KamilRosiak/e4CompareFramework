9ba9d1f5c25aa9a4dce3a79e9b3776e0b805b113
Kamil Rosiak <k.rosiak@tu-bs.de>
2020-10-23 11:34:21 +0200

    settings

--- de.tu_bs.cs.isf.e4cf.compare/src/de/tu_bs/cs/isf/e4cf/compare/CompareEngine.java
+++ de.tu_bs.cs.isf.e4cf.compare/src/de/tu_bs/cs/isf/e4cf/compare/CompareEngine.java
@@ -16,90 +16,103 @@ import de.tu_bs.cs.isf.e4cf.compare.matcher.interfaces.Matcher;
 import de.tu_bs.cs.isf.e4cf.compare.metric.interfaces.Metric;
 
 public class CompareEngine {
-	private Matcher matcher;
-	private Metric metric;
+    private Matcher matcher;
+    private Metric metric;
 
-	public void compare(Tree firstArtifact, Tree secondArtifact, Metric comparisonMetric, Matcher matcher) {
-	    setMetric(comparisonMetric);
-	    setMatcher(matcher);
-	    
-	    Result result = compare(firstArtifact.getRoot(), secondArtifact.getRoot());
-	    
-	}
-	
-	public Result compare(Node firstNode, Node secondNode) {
-	    Set<String> nodeTypes = getNodeTypes(firstNode, secondNode);
-	    
-	    /**
-	     * Compare only nodes with the same type.
-	     */
-	    for(String nodeType : nodeTypes) {
-		List<Node> firstArtifacts = firstNode.getChildrenOfType(nodeType);
-		List<Node> secondArtifacts = secondNode.getChildrenOfType(nodeType);
-		List<NodeComparator> comparator = metric.getComparatorForNodeType(nodeType);
-				
-		for(Node leftArtifact : firstArtifacts) {
-		    for(Node rightArtifact : secondArtifacts) {
-			
+    public void compare(Tree firstArtifact, Tree secondArtifact, Metric comparisonMetric, Matcher matcher) {
+	setMetric(comparisonMetric);
+	setMatcher(matcher);
 
-		    }   
-		}
+	Result result = compare(firstArtifact.getRoot(), secondArtifact.getRoot());
 
-	    }
-	    return null;
-	}
+    }
+
+    public Result compare(Node firstNode, Node secondNode) {
+	Set<String> nodeTypes = getNodeTypes(firstNode, secondNode);
+
 	/**
-	 * This method returns a set that contains all nodes types that are contained in this nodes.
-	 * @return a set of contained node types.
+	 * Compare only nodes with the same type.
 	 */
-	private Set<String> getNodeTypes(Node... nodes ) {
-	    Set<String> nodeTypes = new HashSet<String>();
-	    for(Node node : nodes) {
-		nodeTypes.addAll(node.getAllNodeTypes());
-	    }
-	    return nodeTypes;
-	}
+	for (String nodeType : nodeTypes) {
+	    List<Node> firstArtifacts = firstNode.getChildrenOfType(nodeType);
+	    List<Node> secondArtifacts = secondNode.getChildrenOfType(nodeType);
+	    List<NodeComparator> comparator = metric.getComparatorForNodeType(nodeType);
 
-	private Comparison defaultCompare(Node firstNode, Node secondNode) {
-	    List<Comparison> comparisons = new ArrayList<Comparison>();
-		
-		for(Attribute first_attr : firstNode.getAttributes()) {
-			for(Attribute second_attr : secondNode.getAttributes()) {
-			    comparisons.add(new AttrComparison(first_attr,second_attr, first_attr.compare(second_attr)));	
-			}	
+	    for (Node leftArtifact : firstArtifacts) {
+		for (Node rightArtifact : secondArtifacts) {
+
 		}
-		comparisons = matcher.getMatching(comparisons);
-		
-		return null;
-	}
-	
-	private List<Comparison> getMatching(List<Comparison> comparisons) {
-		return matcher.getMatching(comparisons);
-	}
-	
-	/**
-	 * This method compares the node type of two nodes. 
-	 * @return True if types equals else false
-	 */
-	private boolean isSameNodeType(Node firstNode, Node secondNode) {
-		return firstNode.getNodeType().equals(secondNode.getNodeType());
-	}
+	    }
 
-	public Matcher getMatcher() {
-	    return matcher;
 	}
+	return null;
+    }
 
-
-	public void setMatcher(Matcher matcher) {
-	    this.matcher = matcher;
+    /**
+     * This method returns a set that contains all nodes types that are contained in
+     * this nodes.
+     * 
+     * @return a set of contained node types.
+     */
+    private Set<String> getNodeTypes(Node... nodes) {
+	Set<String> nodeTypes = new HashSet<String>();
+	for (Node node : nodes) {
+	    nodeTypes.addAll(node.getAllNodeTypes());
 	}
+	return nodeTypes;
+    }
 
-	public Metric getMetric() {
-	    return metric;
-	}
+    private Comparison defaultCompare(Node firstNode, Node secondNode) {
+	List<Comparison> comparisons = new ArrayList<Comparison>();
 
-	public void setMetric(Metric metric) {
-	    this.metric = metric;
+	for (Attribute first_attr : firstNode.getAttributes()) {
+	    for (Attribute second_attr : secondNode.getAttributes()) {
+		if(isSameAttributeType(first_attr,second_attr)) {
+		    comparisons.add(new AttrComparison(first_attr, second_attr, first_attr.compare(second_attr)));  
+		}
+	    }
 	}
-	
+	comparisons = matcher.getMatching(comparisons);
+
+	return null;
+    }
+
+    private List<Comparison> getMatching(List<Comparison> comparisons) {
+	return matcher.getMatching(comparisons);
+    }
+
+    /**
+     * This method compares the attribute keys of two attributes.
+     * 
+     * @return True if types equals else false
+     */
+    private boolean isSameAttributeType(Attribute firstAttr, Attribute secondAttr) {
+	return firstAttr.getAttributeKey().equals(secondAttr.getAttributeKey());
+    }
+    
+    /**
+     * This method compares the node type of two nodes.
+     * 
+     * @return True if types equals else false
+     */
+    private boolean isSameNodeType(Node firstNode, Node secondNode) {
+	return firstNode.getNodeType().equals(secondNode.getNodeType());
+    }
+
+    public Matcher getMatcher() {
+	return matcher;
+    }
+
+    public void setMatcher(Matcher matcher) {
+	this.matcher = matcher;
+    }
+
+    public Metric getMetric() {
+	return metric;
+    }
+
+    public void setMetric(Metric metric) {
+	this.metric = metric;
+    }
+
 }
