4238e81c4d596dc62807ae738d979c5c236ca194
paulo-haas <git@paulo-haas.de>
2020-11-09 16:26:45 +0100

    Sorted imports alphabetically, resolved name of file for tree name

--- de.tu_bs.cs.isf.e4cf.compare.data_structures/src/de/tu_bs/cs/isf/e4cf/compare/data_structures/io/reader/JavaReader.java
+++ de.tu_bs.cs.isf.e4cf.compare.data_structures/src/de/tu_bs/cs/isf/e4cf/compare/data_structures/io/reader/JavaReader.java
@@ -1,7 +1,7 @@
 package de.tu_bs.cs.isf.e4cf.compare.data_structures.io.reader;
 
-import java.nio.file.Paths;
-
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.impl.NodeImpl;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.impl.TreeImpl;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.AbstractArtifactReader;
@@ -9,8 +9,8 @@ import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.Node;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.Tree;
 import de.tu_bs.cs.isf.e4cf.core.file_structure.FileTreeElement;
 import de.tu_bs.cs.isf.e4cf.core.util.file.FileStreamUtil;
-
-import com.github.javaparser.ast.CompilationUnit;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /***
  * 
@@ -35,6 +35,7 @@ public class JavaReader extends AbstractArtifactReader {
 		
 		if (isFileSupported(element)) {
 			String s = FileStreamUtil.readLineByLine(Paths.get(element.getAbsolutePath()));
+			String fileName = Paths.get(element.getAbsolutePath()).getFileName().toString();
 			CompilationUnit cu = StaticJavaParser.parse(s);
 			// do stuff
 			
