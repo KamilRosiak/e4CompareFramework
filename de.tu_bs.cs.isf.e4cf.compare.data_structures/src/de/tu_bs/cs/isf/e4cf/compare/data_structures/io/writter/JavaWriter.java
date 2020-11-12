3f56ec93964f2e64ee7e3aff94b08232f5f6ad12
paulo-haas <git@paulo-haas.de>
2020-11-12 13:51:12 +0100

    Multiples small changes to JavaWriter
    Added javadocs
    FileGeneration is now recursively
    Added some comments

--- de.tu_bs.cs.isf.e4cf.compare.data_structures/src/de/tu_bs/cs/isf/e4cf/compare/data_structures/io/writter/JavaWriter.java
+++ de.tu_bs.cs.isf.e4cf.compare.data_structures/src/de/tu_bs/cs/isf/e4cf/compare/data_structures/io/writter/JavaWriter.java
@@ -7,7 +7,8 @@ import de.tu_bs.cs.isf.e4cf.core.util.file.FileStreamUtil;
 
 /**
  * 
- * This class converts the generic tree structure back into the original java file.  
+ * This class converts the generic tree structure back into the original java
+ * file.
  * 
  * @author Serkan Acar
  * @author Pascal Blum
@@ -15,31 +16,50 @@ import de.tu_bs.cs.isf.e4cf.core.util.file.FileStreamUtil;
  * @author Hassan Smaoui
  *
  */
-
 public class JavaWriter extends AbstractArtifactWriter {
 	public final static String FILE_ENDING = "java";
 	public final static String NODE_TYPE_TREE = "JAVA";
 
+	/**
+	 * Initializes a new instance of class JavaWriter.
+	 */
 	public JavaWriter() {
 		super(FILE_ENDING);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String getSuppotedNodeType() {
 		return NODE_TYPE_TREE;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void writeArtifact(Tree tree, String path) {
 		// TODO Auto-generated method stub
-		if(tree.getArtifactType().equals(NODE_TYPE_TREE)) {
-			FileStreamUtil.writeTextToFile(path +"."+FILE_ENDING, generateFileContent(tree.getRoot()));
+		if (tree.getArtifactType().equals(NODE_TYPE_TREE)) {
+			FileStreamUtil.writeTextToFile(path + "." + FILE_ENDING, generateFileContent(tree.getRoot()));
 		}
 	}
 
+	/**
+	 * Generates the contents of a java source file recursively.
+	 * 
+	 * @param root Node of the syntax tree.
+	 * @return Contents of a file
+	 */
 	private String generateFileContent(Node root) {
 		String fileContent = new String();
-		for(Node node : root.getChildren()) {
+
+		// Build the file depth-first
+		// If node is leaf then getChildren returns an empty list and the body of the
+		// for loop is not executed.
+		for (Node node : root.getChildren()) {
+			fileContent += generateFileContent(node);
 		}
 		return fileContent;
 	}
