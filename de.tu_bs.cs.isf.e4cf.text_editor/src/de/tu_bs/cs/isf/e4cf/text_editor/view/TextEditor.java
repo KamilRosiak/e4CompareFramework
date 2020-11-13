a3fa68ebfc810266f27edfc73dd30f0f13e66785
CedricKapalla <keeper596@freenet.de>
2020-11-13 13:13:18 +0100

    Comments for most of the menu items and functions.

--- de.tu_bs.cs.isf.e4cf.text_editor/src/de/tu_bs/cs/isf/e4cf/text_editor/view/TextEditor.java
+++ de.tu_bs.cs.isf.e4cf.text_editor/src/de/tu_bs/cs/isf/e4cf/text_editor/view/TextEditor.java
@@ -91,78 +91,142 @@ public class TextEditor implements Initializable {
 		});
 	}
 
-	
+	/**
+	 * Initialises the Edit menu. Includes undo/redo, copy/paste/cut, Delete, and Select All
+	 * 
+	 * @author Cedric Kapalla
+	 */
 	private void initEditMenuItems() {
-		initEditMenuItemUndoAction(); 
-		initEditMenuItemRedoAction();
-		initEditMenuItemCutTextAction();
-		initEditMenuItemCopyTextAction();
-		initEditMenuItemPasteTextAction();
-		initEditMenuItemDeleteTextAction(); 
-		initEditMenuItemSelectAllAction(); 
+		initEditMenuItemUndoAction();	//undo
+		initEditMenuItemRedoAction();	//redo
+		initEditMenuItemCutTextAction();	//cut
+		initEditMenuItemCopyTextAction();	//copy
+		initEditMenuItemPasteTextAction();	//paste
+		initEditMenuItemDeleteTextAction();	 //delete
+		initEditMenuItemSelectAllAction(); 	//select all
 	}
 	
+	/**
+	 * Sets the actions of the Undo item in the Edit menu.
+	 * Undoes the most recent actions taken in the editor.
+	 * 
+	 * @author Cedric Kapalla
+	 */
 	private void initEditMenuItemUndoAction() {
-		undo.setOnAction(e -> {
-			System.out.println("Undo text or action.");
-		});
+		undo.setOnAction(e -> {		//activate once pressed
+			System.out.println("Undo text or action.");	
+		}); //currently a placeholder
 	}
 
+	/**
+	 * Sets the actions of the Redo item in the Edit menu.
+	 * Redoes an action previously undone.
+	 * 
+	 * @author Cedric Kapalla
+	 */
 	private void initEditMenuItemRedoAction() {
 		redo.setOnAction(e -> {
-			System.out.println("Redo text or action once undone.");
-		});
+			System.out.println("Redo text or action once undone."); 
+		}); //currently a placeholder
 	}
-	
+
+	/**
+	 * Sets the actions of the Cut item in the Edit menu.
+	 * Cuts out the selected text from the editor window.
+	 * 
+	 * @author Cedric Kapalla
+	 */
 	private void initEditMenuItemCutTextAction() {
 		cutText.setOnAction(e -> {
 			System.out.println("Cut out selected text.");
-		});
+		}); //currently a placeholder
 	}
 	
+	/**
+	 * Sets the actions of the Copy item in the Edit menu.
+	 * Copies the selected text in the editor window.
+	 * 
+	 * @author Cedric Kapalla
+	 */
 	private void initEditMenuItemCopyTextAction() {
 		copyText.setOnAction(e -> {
 			System.out.println("Copy selected text.");
-		});
+		}); //currently a placeholder
 	}
 	
+	/**
+	 * Sets the actions of the Paste item in the Edit menu.
+	 * Pastes text from clipboard into the editor window.
+	 * 
+	 * @author Cedric Kapalla
+	 */
 	private void initEditMenuItemPasteTextAction() {
 		pasteText.setOnAction(e -> {
 			System.out.println("Paste text on clipboard.");
-		});
+		}); //currently a placeholder
 	}
 	
+	/**
+	 * Sets the actions of the Delete item in the Edit menu.
+	 * Deletes the selected text in the editor window.
+	 * 
+	 * @author Cedric Kapalla
+	 */
 	private void initEditMenuItemDeleteTextAction() {
 		deleteText.setOnAction(e -> {
 			System.out.println("Delete selected text.");
-		});
+		}); //currently a placeholder
 	}
 	
+	/**
+	 * Sets the actions of the Select All item in the Edit menu.
+	 * Selects all text open in the editor window.
+	 * 
+	 * @author Cedric Kapalla
+	 */
 	private void initEditMenuItemSelectAllAction() {
 		selectAllText.setOnAction(e -> {
 			System.out.println("Select all text.");
-		});
+		}); //currently a placeholder
 	}
 	
-	
+	/**
+	 * This function initialises the Extra menu. It consists of the 'Preference' item
+	 * 
+	 * @author Cedric Kapalla
+	 */
 	private void initExtraMenuItems() {
-		initExtraMenuItemPreferenceAction();  
+		initExtraMenuItemPreferenceAction();  //preferences
 	}
 	
+	/**
+	 * Sets the actions of the Preferences item in the Extra menu.
+	 * 
+	 * @author Cedric Kapalla
+	 */
 	private void initExtraMenuItemPreferenceAction() {
 		preferences.setOnAction(e -> {
 			System.out.println("Adjust Preferences");
-		});
+		}); //currently a placeholder
 	}
 	
-	
+	/**
+	 * This function initialises the Help menu. It consists of the 'Preference' item
+	 * 
+	 * @author Cedric Kapalla
+	 */
 	private void initHelpMenuItems() {
-		initHelpMenuItemAboutAction();  
+		initHelpMenuItemAboutAction();  //about
 	}
 	
+	/**
+	 * Sets the actions of the About item in the Help menu
+	 * 
+	 * @author Cedric Kapalla
+	 */
 	private void initHelpMenuItemAboutAction() {
 		about.setOnAction(e -> {
 			System.out.println("Placeholder. There is no help for you :(");
-		});
+		}); //currently a placeholder
 	}
 }
