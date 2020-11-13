315110b7a836862eda3682f1f1720e5d6ce1ccb5
erwin97 <erwinwijaya@gmx.de>
2020-11-13 21:23:19 +0100

    Implementation of Open,Save,SaveAs,New

---
+++ de.tu_bs.cs.isf.e4cf.text_editor/src/de/tu_bs/cs/isf/e4cf/text_editor/FileUtils.java
@@ -0,0 +1,141 @@
+package de.tu_bs.cs.isf.e4cf.text_editor;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Window;
+
+
+public class FileUtils {
+	private FileChooser fileChooser;
+	private Window parent;
+	private File file;
+	private String lastSavedRevision;
+	
+	/**
+	 * A Method to initialize fileChooser and the extension that can be choose
+	 * 
+	 * @author Lukas Cronauer, Erwin Wijaya
+	 */
+	public FileUtils(Window parent) {
+		this.parent = parent;
+
+		fileChooser = new FileChooser();
+		fileChooser.setTitle("Open a File");
+		fileChooser.getExtensionFilters().addAll(
+			new ExtensionFilter("Text Files", "*.txt"),
+			new ExtensionFilter("Java Files", "*.java"),
+			new ExtensionFilter("XML Files", "*.xml")
+		);
+		fileChooser.setInitialDirectory(new File("."));
+	}
+
+	/**
+	 * A Method for opening a file
+	 * 
+	 * @author Lukas Cronauer
+	 */
+	public String openFile() {
+		File f = fileChooser.showOpenDialog(parent);
+		if (f == null) {
+			return "";
+		} else if (!f.exists()) {
+			// show error dialog
+			return "";
+		}
+
+		file = f;
+		return readFile(file);	
+	}
+	
+	/**
+	 * A Method to read the file and return the string
+	 * 
+	 * @author Lukas Cronauer, Erwin Wijaya
+	 */
+	public String readFile(File file) {
+		FileReader reader;
+		try {
+			reader = new FileReader(file);
+			int character;
+			String text="";
+			
+			while((character = reader.read()) != -1) {
+				text += (char) character;
+			}
+			reader.close();
+			lastSavedRevision = text;
+			return text;
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException io) {
+			io.printStackTrace();
+		}
+		return "";
+	}
+	
+	/**
+	 * A Method to save a file that is currently in use
+	 * 
+	 * @author Lukas Cronauer, Erwin Wijaya
+	 */
+	public boolean save(String content) {
+		if (file == null) {
+			saveAs(content);
+		}
+
+		return writeFile(content);
+	}
+
+	/**
+	 * A Method to save a file in another directory or with another name
+	 * 
+	 * @author Lukas Cronauer, Erwin Wijaya
+	 */
+	public boolean saveAs(String content) {
+		File f = fileChooser.showSaveDialog(parent);
+		if (f == null) {
+			return false;
+		} else {
+			file = f;
+		}
+
+		return writeFile(content);
+	}
+
+	/**
+	 * A Method to write the File and saving it
+	 * 
+	 * @author Lukas Cronauer, Erwin Wijaya
+	 */
+	private boolean writeFile(String content) {
+		fileChooser.setTitle("Save a File");
+		FileWriter writer;
+		try {
+			writer = new FileWriter(file.getName(), false);
+			writer.write(content);
+			writer.close();
+			lastSavedRevision = content;
+			return true;
+		} catch(IOException io) {
+			io.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * A Method to get latest Revision
+	 * 
+	 * @author Lukas Cronauer
+	 */
+	public String getLastRevision() {
+		return lastSavedRevision;
+	}
+}
+	
+
--- de.tu_bs.cs.isf.e4cf.text_editor/src/de/tu_bs/cs/isf/e4cf/text_editor/view/TextEditor.java
+++ de.tu_bs.cs.isf.e4cf.text_editor/src/de/tu_bs/cs/isf/e4cf/text_editor/view/TextEditor.java
@@ -1,5 +1,370 @@
 package de.tu_bs.cs.isf.e4cf.text_editor.view;
 
-public class TextEditor {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javax.inject.Inject;
+import org.apache.commons.lang3.StringUtils;
+
+import de.tu_bs.cs.isf.e4cf.core.util.ServiceContainer;
+import de.tu_bs.cs.isf.e4cf.text_editor.FileUtils;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.IndexRange;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DataFormat;
+
+
+
+/**
+ * 
+ * @author Sören Christmann, Cedric Kapalla, Lukas Cronauer, Erwin Wijaya
+ *
+ */
+public class TextEditor implements Initializable {
+	//File Menu
+	@FXML private MenuItem newFile;
+	@FXML private MenuItem openFile;
+	@FXML private MenuItem saveFile;
+	@FXML private MenuItem saveFileAs;
+	@FXML private MenuItem closeFile;
+	@FXML private MenuItem closeEditor;
+	
+	//Edit Menu
+	@FXML private MenuItem undo;
+	@FXML private MenuItem redo;
+	@FXML private MenuItem cutText;
+	@FXML private MenuItem copyText;
+	@FXML private MenuItem pasteText;
+	@FXML private MenuItem deleteText;
+	@FXML private MenuItem selectAllText;
+	
+	//Extra Menu
+	@FXML private MenuItem preferences;
+	
+	//Help Menu
+	@FXML private MenuItem about;
+	
+	//Text area set up
+	@FXML private TextArea textArea;
+	Clipboard systemClipboard = Clipboard.getSystemClipboard();
+	
+	@Inject private ServiceContainer services;
+
+	//Utils class to handle file operations
+	FileUtils fileUtils;
+	
+	//Scene of this object
+	Scene scene;
+	
+	
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		initFileMenuItems();
+		initEditMenuItems();
+		initExtraMenuItems();
+		initHelpMenuItems();
+	}
+	
+	private void initFileMenuItems() {
+		initFileMenuItemNewAction(); 
+		initFileMenuItemOpenAction();
+		initFileMenuItemSaveAction();
+		initFileMenuItemSaveAsAction();
+		initFileMenuItemCloseFileAction(); // Closes the current File
+		initFileMenuItemCloseEditorAction(); //Closes the Editor Window
+		
+	}
+	
+	/**
+	 * Sets the actions of the New item in the File menu.
+	 * Make a new File
+	 * 
+	 * @author Lukas Cronauer, Erwin Wijaya
+	 */
+	private void initFileMenuItemNewAction() {
+		newFile.setOnAction(e -> {
+			textArea.setText(null);
+			System.out.println("New");
+		});
+	}
+	
+	/**
+	 * Sets the actions of the Open item in the File menu.
+	 * Open a File with given extension
+	 * 
+	 * @author Lukas Cronauer, Erwin Wijaya
+	 */
+	private void initFileMenuItemOpenAction() {
+		openFile.setOnAction(e -> {
+			System.out.println("Open");
+			String content = fileUtils.openFile();
+			if (!content.equals("")) {
+			 	textArea.setText(content);
+			}
+		});
+	}
+	
+	/**
+	 * Sets the actions of the Save item in the File menu.
+	 * Saving the text into current File
+	 * 
+	 * @author Lukas Cronauer, Erwin Wijaya
+	 */
+	private void initFileMenuItemSaveAction() {
+		saveFile.setOnAction(e -> {
+			System.out.println("Save");
+			String content = textArea.getText();
+			if (!fileUtils.save(content)) {
+				// show error dialog
+			}
+		});
+	}
+	
+	/**
+	 * Sets the actions of the SaveAs item in the File menu.
+	 * Make a copy of the file in another file directory  
+	 * Or make a copy of the file with another name
+	 * 
+	 * @author Lukas Cronauer, Erwin Wijaya
+	 */
+	private void initFileMenuItemSaveAsAction() {
+		saveFileAs.setOnAction(e -> {
+			System.out.println("Save As");
+			String content = textArea.getText();
+			if (!fileUtils.saveAs(content)) {
+				// show error dialog
+			}
+		});
+	}
+	
+	/**
+	 * Sets the actions of the CloseItem item in the File menu.
+	 * Closing the File that are currently open.
+	 * 
+	 * @author Lukas Cronauer, Erwin Wijaya
+	 */
+	private void initFileMenuItemCloseFileAction() {
+		closeFile.setOnAction(e -> {
+			System.out.println("Close File");
+			saveChanges();
+		});
+	}
+	
+	/**
+	 * Sets the actions of the CloseEditor item in the File menu.
+	 * Closing the whole Editor.
+	 * 
+	 * @author Lukas Cronauer, Erwin Wijaya
+	 */
+	private void initFileMenuItemCloseEditorAction() {
+		closeEditor.setOnAction(e -> {
+			System.out.println("Close Editor");
+			saveChanges();
+		});
+	}
+
+	/**
+	 * Sets the actions of the Undo item in the Edit menu.
+	 * Undoes the most recent actions taken in the editor.
+	 * 
+	 * @author Lukas Cronauer
+	 */
+	private void saveChanges() {
+		String content = textArea.getText();
+		if (!content.equals(fileUtils.getLastRevision())) {
+			fileUtils.save(content);
+		}
+	}
+	
+	/**
+	 * Sets the actions of the Undo item in the Edit menu.
+	 * Initialize a method with the scene
+	 * 
+	 * @author Lukas Cronauer
+	 */
+	public void initFileUtils(Scene scene) {
+		 this.scene = scene;
+		 if (scene != null) {
+			fileUtils = new FileUtils(scene.getWindow());
+		 }
+	}
+
+	/**
+	 * Initializes the Edit menu. Includes undo/redo, copy/paste/cut, Delete, and Select All
+	 * 
+	 * @author Cedric Kapalla
+	 */
+	private void initEditMenuItems() {
+		initEditMenuItemUndoAction();	//undo
+		initEditMenuItemRedoAction();	//redo
+		initEditMenuItemCutTextAction();	//cut
+		initEditMenuItemCopyTextAction();	//copy
+		initEditMenuItemPasteTextAction();	//paste
+		initEditMenuItemDeleteTextAction();	 //delete
+		initEditMenuItemSelectAllAction(); 	//select all
+	}
+	
+	/**
+	 * Sets the actions of the Undo item in the Edit menu.
+	 * Undoes the most recent actions taken in the editor.
+	 * 
+	 * @author Cedric Kapalla
+	 */
+	private void initEditMenuItemUndoAction() {
+		undo.setOnAction(e -> {		//activate once pressed
+			System.out.println("Undo text or action.");	
+		}); //currently a placeholder
+	}
+
+	/**
+	 * Sets the actions of the Redo item in the Edit menu.
+	 * Redoes an action previously undone.
+	 * 
+	 * @author Cedric Kapalla
+	 */
+	private void initEditMenuItemRedoAction() {
+		redo.setOnAction(e -> {
+			System.out.println("Redo text or action once undone."); 
+		}); //currently a placeholder
+	}
+
+	/**
+	 * Sets the actions of the Cut item in the Edit menu.
+	 * Cuts out the selected text from the editor window.
+	 * 
+	 * @author Cedric Kapalla
+	 */
+	private void initEditMenuItemCutTextAction() {
+		cutText.setOnAction(e -> {
+			ClipboardContent text = new ClipboardContent();
+			text.putString(textArea.getSelectedText());	//add selected text to clipboard content
+			systemClipboard.setContent(text);	//add content to clipboard
+			textArea.deleteText(textArea.getSelection());
+		});
+	}
+	
+	/**
+	 * Sets the actions of the Copy item in the Edit menu.
+	 * Copies the selected text in the editor window.
+	 * 
+	 * @author Cedric Kapalla
+	 */
+	private void initEditMenuItemCopyTextAction() {
+		copyText.setOnAction(e -> {
+			ClipboardContent text = new ClipboardContent();	//add selected text to clipboard content
+			text.putString(textArea.getSelectedText());	//add content to clipboard
+			systemClipboard.setContent(text);
+		});
+	}
+	
+	/**
+	 * Sets the actions of the Paste item in the Edit menu.
+	 * Pastes text from clipboard into the editor window by reading it from the clipboard and then
+	 * composing a new string around this content.
+	 * 
+	 * @author Cedric Kapalla
+	 */
+	private void initEditMenuItemPasteTextAction() {
+		pasteText.setOnAction(e -> {
+			if( !systemClipboard.hasContent(DataFormat.PLAIN_TEXT) ) {
+			   return;	//does nothing if there is nothing or no text on clipboard
+			}
+			
+			String pasteText = systemClipboard.getString();	//get text from Clipboard
+			IndexRange range = textArea.getSelection();	//finds cursor position and selected text to overwrite
+			
+			//initialisation of variables
+			int endPos = 0;	//defines where the cursor will be after paste action
+			String updatedText = "";
+			String origText = textArea.getText();
+			
+			//Separates the points before and after the inserted text's position
+			String firstPart = StringUtils.substring (origText, 0, range.getStart());
+			String lastPart = StringUtils.substring(origText, range.getEnd(), StringUtils.length(origText));
+			
+			//puts together the new String
+			updatedText = firstPart + pasteText + lastPart;
+			
+			//checks for where to put the cursor after adding text in
+			if (range.getStart() == range.getEnd()) {
+				endPos = range.getEnd() + StringUtils.length(pasteText);
+			} else {
+				endPos = range.getStart() + StringUtils.length(pasteText);
+			}
+			
+			textArea.setText(updatedText);
+			textArea.positionCaret(endPos);
+		}); 
+	}
+	
+	/**
+	 * Sets the actions of the Delete item in the Edit menu.
+	 * Deletes the selected text in the editor window.
+	 * 
+	 * @author Cedric Kapalla
+	 */
+	private void initEditMenuItemDeleteTextAction() {
+		deleteText.setOnAction(e -> {
+			textArea.deleteText(textArea.getSelection());
+		});
+	}
+	
+	/**
+	 * Sets the actions of the Select All item in the Edit menu.
+	 * Selects all text open in the editor window.
+	 * 
+	 * @author Cedric Kapalla
+	 */
+	private void initEditMenuItemSelectAllAction() {
+		selectAllText.setOnAction(e -> {
+			System.out.println("Select all text.");
+		}); //currently a placeholder
+	}
+	
+	/**
+	 * This function initializes the Extra menu. It consists of the 'Preference' item
+	 * 
+	 * @author Cedric Kapalla
+	 */
+	private void initExtraMenuItems() {
+		initExtraMenuItemPreferenceAction();  //preferences
+	}
+	
+	/**
+	 * Sets the actions of the Preferences item in the Extra menu.
+	 * 
+	 * @author Cedric Kapalla
+	 */
+	private void initExtraMenuItemPreferenceAction() {
+		preferences.setOnAction(e -> {
+			System.out.println("Adjust Preferences");
+		}); //currently a placeholder
+	}
+	
+	/**
+	 * This function initializes the Help menu. It consists of the 'Preference' item
+	 * 
+	 * @author Cedric Kapalla
+	 */
+	private void initHelpMenuItems() {
+		initHelpMenuItemAboutAction();  //about
+	}
+	
+	/**
+	 * Sets the actions of the About item in the Help menu
+	 * 
+	 * @author Cedric Kapalla
+	 */
+	private void initHelpMenuItemAboutAction() {
+		about.setOnAction(e -> {
+			System.out.println("Placeholder. There is no help for you :(");
+		}); //currently a placeholder
+	}
 }
