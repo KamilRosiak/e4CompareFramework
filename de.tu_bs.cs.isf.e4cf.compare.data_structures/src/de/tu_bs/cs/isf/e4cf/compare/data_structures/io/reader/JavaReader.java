abef221a8539da77e7c887e1ad2f154d9dc061e8
paulo-haas <git@paulo-haas.de>
2020-11-16 17:13:52 +0100

    fixed assignment of simpleName

--- de.tu_bs.cs.isf.e4cf.compare.data_structures/src/de/tu_bs/cs/isf/e4cf/compare/data_structures/io/reader/JavaReader.java
+++ de.tu_bs.cs.isf.e4cf.compare.data_structures/src/de/tu_bs/cs/isf/e4cf/compare/data_structures/io/reader/JavaReader.java
@@ -11,6 +11,8 @@ import de.tu_bs.cs.isf.e4cf.core.file_structure.FileTreeElement;
 import de.tu_bs.cs.isf.e4cf.core.util.file.FileStreamUtil;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.ImportDeclaration;
@@ -18,6 +20,7 @@ import com.github.javaparser.ast.PackageDeclaration;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.expr.SimpleName;
+
 /***
  * 
  * @author Serkan Acar
@@ -33,52 +36,59 @@ public class JavaReader extends AbstractArtifactReader {
 	private Node recursivelyTreeBuilder(com.github.javaparser.ast.Node node) {
 		// create a node
 		Node newNode = new NodeImpl(String.valueOf(node.getClass()));
-		
+
 		// counters
 		int modifierCtr = 0;
 
 		// search for imports, packages (only for compilation unit)
 		if (node.getClass().equals(CompilationUnit.class)) {
-			newNode.addAttribute("" + JavaNodeTypes.Package, ((CompilationUnit) node).getPackageDeclaration().toString());
+			newNode.addAttribute("" + JavaNodeTypes.Package,
+					((CompilationUnit) node).getPackageDeclaration().toString());
 		}
-		
+
 		// set Attributes (Type, SimpleName, Modifier)
 		newNode.addAttribute("Type", node.getClass().toString());
-		newNode.addAttribute("SimpleName", node.toString());
+
+		// Filter the child nodes to a list only containing node of type SimpleName. The
+		// list should contain zero or one element. If the node has a child of type
+		// SimpleName add the name as a attribute to the new node.
+		List<com.github.javaparser.ast.Node> simpleNameList = node.getChildNodes().stream()
+				.filter(n -> n.getClass().equals(SimpleName.class)).collect(Collectors.toList());
+		if (simpleNameList.size() > 0) {
+			newNode.addAttribute(SimpleName.class.toString(), ((SimpleName) simpleNameList.get(0)).asString());
+		}
+
 		if (node.getClass().equals(ClassOrInterfaceDeclaration.class)) {
 			for (Modifier modifier : ((ClassOrInterfaceDeclaration) node).getModifiers()) {
 				newNode.addAttribute("Modifier" + modifierCtr, modifier.toString());
 				modifierCtr++;
 			}
 		}
-		
-				
 
 		// Recursive depth search behavior
-		for (int i = 0; i < node.getChildNodes().size(); i++) {	
+		for (int i = 0; i < node.getChildNodes().size(); i++) {
 			com.github.javaparser.ast.Node child = node.getChildNodes().get(i);
-			
+
 			if (child.getClass().equals(Modifier.class)) {
 				node.remove(child);
-			}
-			else if (child.getClass().equals(SimpleName.class)) {
+			} else if (child.getClass().equals(SimpleName.class)) {
 				node.remove(child);
 			}
-			/*else if (child.getClass().equals()) {
-				
-			}
-			else if (child.getClass().equals()) {
-				
-			}
-			else if (child.getClass().equals()) {
-				
-			}*/
+			/*
+			 * else if (child.getClass().equals()) {
+			 * 
+			 * } else if (child.getClass().equals()) {
+			 * 
+			 * } else if (child.getClass().equals()) {
+			 * 
+			 * }
+			 */
 			else {
 				Node newChildNode = recursivelyTreeBuilder(child);
-				newNode.addChild(newChildNode);	
-			}		
+				newNode.addChild(newChildNode);
+			}
 		}
-		
+
 		newNode.addAttribute("columnBegin",
 				String.valueOf(node.getTokenRange().get().getBegin().getRange().get().begin.column));
 		newNode.addAttribute("columnEnd",
@@ -89,7 +99,7 @@ public class JavaReader extends AbstractArtifactReader {
 				String.valueOf(node.getTokenRange().get().getBegin().getRange().get().end.line));
 
 		newNode.addAttribute("ModifierCount", "" + modifierCtr);
-		
+
 		return newNode;
 	}
 
@@ -105,7 +115,7 @@ public class JavaReader extends AbstractArtifactReader {
 			String s = FileStreamUtil.readLineByLine(Paths.get(element.getAbsolutePath()));
 			String fileName = Paths.get(element.getAbsolutePath()).getFileName().toString();
 			CompilationUnit cu = StaticJavaParser.parse(s);
-			
+
 			tree = new TreeImpl(fileName, recursivelyTreeBuilder(cu));
 		}
 
