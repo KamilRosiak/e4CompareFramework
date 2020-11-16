4e8a127255d811dc2ed0a76711f992674a00a045
paulo-haas <git@paulo-haas.de>
2020-11-16 17:52:50 +0100

    Changed if else structure to multiple condition

--- de.tu_bs.cs.isf.e4cf.compare.data_structures/src/de/tu_bs/cs/isf/e4cf/compare/data_structures/io/reader/JavaReader.java
+++ de.tu_bs.cs.isf.e4cf.compare.data_structures/src/de/tu_bs/cs/isf/e4cf/compare/data_structures/io/reader/JavaReader.java
@@ -69,14 +69,10 @@ public class JavaReader extends AbstractArtifactReader {
 		for (int i = 0; i < node.getChildNodes().size(); i++) {
 			com.github.javaparser.ast.Node child = node.getChildNodes().get(i);
 
-			if (child.getClass().equals(Modifier.class)) {
+			if (child.getClass().equals(Modifier.class) || child.getClass().equals(SimpleName.class)
+					|| child.getClass().equals(PackageDeclaration.class)) {
 				node.remove(child);
-			} else if (child.getClass().equals(SimpleName.class)) {
-				node.remove(child);
-			} else if (child.getClass().equals(PackageDeclaration.class)) {
-				node.remove(child);
-			}
-			else {
+			} else {
 				Node newChildNode = recursivelyTreeBuilder(child);
 				newNode.addChild(newChildNode);
 			}
