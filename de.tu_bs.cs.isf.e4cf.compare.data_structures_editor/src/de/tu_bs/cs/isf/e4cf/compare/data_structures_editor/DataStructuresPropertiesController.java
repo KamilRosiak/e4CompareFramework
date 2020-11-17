00e482e37c697fab1112877bf87934643f8112c9
Niklas Bärthel <n.baerthel@tu-braunschweig.de>
2020-11-17 12:33:15 +0100

    GUI in Framework eingebaut

---
+++ de.tu_bs.cs.isf.e4cf.compare.data_structures_editor/src/de/tu_bs/cs/isf/e4cf/compare/data_structures_editor/DataStructuresPropertiesController.java
@@ -0,0 +1,60 @@
+package de.tu_bs.cs.isf.e4cf.compare.data_structures_editor;
+ 
+
+import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.di.Persist;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+
+import de.tu_bs.cs.isf.e4cf.compare.data_structures_editor.view.PropertiesView;
+import de.tu_bs.cs.isf.e4cf.compare.data_structures_editor.view.TreeView;
+import de.tu_bs.cs.isf.e4cf.core.gui.java_fx.util.FXMLLoader;
+import de.tu_bs.cs.isf.e4cf.core.util.ServiceContainer;
+import javafx.embed.swt.FXCanvas;
+import javafx.scene.Scene;
+
+public class DataStructuresPropertiesController {
+	private static final String TREE_VIEW_FXML = "ui/view/Properties.fxml";
+	public static final String COMPARATOR_VIEW_CSS_LOCATION = "css/comparator_view.css";
+	public static final String BUNDLE_NAME = "de.tu_bs.cs.isf.e4cf.compare.data_structures_editor";
+	
+	@Inject
+	public DataStructuresPropertiesController(Composite parent, ServiceContainer services, IEclipseContext context) {
+		postConstruct(parent, services, context);
+	}
+	@PostConstruct
+	public void postConstruct(Composite parent, ServiceContainer services, IEclipseContext context) {
+		System.out.println("Ich funktioniere.");
+		FXCanvas canvans = new FXCanvas(parent, SWT.None);
+		FXMLLoader<PropertiesView> loader = new FXMLLoader<PropertiesView>(context, BUNDLE_NAME, TREE_VIEW_FXML);
+
+		Scene scene = new Scene(loader.getNode());
+//        scene.getStylesheets().add(COMPARATOR_VIEW_CSS_LOCATION);
+		canvans.setScene(scene);
+
+	}
+
+	@PreDestroy
+	public void preDestroy() {
+
+	}
+
+	@Focus
+	public void onFocus() {
+	
+	}
+
+	@Persist
+	public void save() {
+
+	}
+	
+	
+	
+	
+}
