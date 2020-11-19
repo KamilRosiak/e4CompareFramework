f19f99b04cbe313ea0322ae917c263883ed95ce0
Rami Jaballi <rami.jaballi@tu-braunschweig.de>
2020-11-19 10:54:07 +0100

    generic createtable-method implemented

--- de.tu_bs.cs.isf.e4cf.core/src/de/tu_bs/cs/isf/e4cf/core/db/ITableService.java
+++ de.tu_bs.cs.isf.e4cf.core/src/de/tu_bs/cs/isf/e4cf/core/db/ITableService.java
@@ -1,10 +1,20 @@
 package de.tu_bs.cs.isf.e4cf.core.db;
 
+import java.sql.SQLException;
+
 public interface ITableService {
 
 	void createTable(String pPath, String pDbName, String tableName, Column... attributs);
 
-	void createTable(String pPath, String pDbName, Class<?> cls);
+	/**
+	 * Method to create a table from a java-class in a generic way.
+	 * 
+	 * @param pPath   String the path of the database
+	 * @param pDbName String the name of the database
+	 * @param cls     the class from which the table will be created
+	 * @throws SQLException 
+	 */
+	void createTable(String pPath, String pDbName, Class<?> cls) throws SQLException;
 
 	void deleteTable(String pPath, String pDbName, String tableName);
 
@@ -19,9 +29,9 @@ public interface ITableService {
 	void dropPrimaryKey(String pPath, String pDbName, String tableName, String... columnNames);
 
 	void makeColumnAutoIncrement(String pPath, String pDbName, String tableName, String columnNames);
-	
+
 	void makeColumnUnique(String pPath, String pDbName, String tableName, String columnNames);
-	
+
 	void makeColumnNotNull(String pPath, String pDbName, String tableName, String columnNames);
 
 }
--- de.tu_bs.cs.isf.e4cf.core/src/de/tu_bs/cs/isf/e4cf/core/db/TableServiceImp.java
+++ de.tu_bs.cs.isf.e4cf.core/src/de/tu_bs/cs/isf/e4cf/core/db/TableServiceImp.java
@@ -1,60 +1,98 @@
 package de.tu_bs.cs.isf.e4cf.core.db;
 
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class TableServiceImp extends TableUtilities implements ITableService {
 
 	@Override
-	public void createTable(final String pPath, final String pDbName, final String tableName, final Column... attributs) {
+	public void createTable(final String pPath, final String pDbName, final String tableName,
+			final Column... attributs) {
 		// Jessica
 	}
 
+	/**
+	 * Method to create a table from a java-class in a generic way.
+	 * 
+	 * @param pPath   String the path of the database
+	 * @param pDbName String the name of the database
+	 * @param cls     the class from which the table will be created
+	 * @throws SQLException
+	 */
 	@Override
-	public void createTable(final String pPath, final String pDbName, final Class<?> cls) {
-		//Rami
+	public void createTable(final String pPath, final String pDbName, final Class<?> cls) throws SQLException {
+		final Connection con = DatabaseFactory.getInstance().getDatabase(pPath, pDbName);
+		final Statement s = con.createStatement();
+		final String tableName = cls.getSimpleName();
+		if (!tableExists(pPath, pDbName, cls.getSimpleName())) {
+			String sqlStatment = "CREATE TABLE " + tableName + " (";
+			// get all attribute objects from the java-class
+			final Field[] fieldlist = cls.getDeclaredFields();
+			for (final Field aFieldlist : fieldlist) {
+				sqlStatment += aFieldlist.getName() + " " + aFieldlist.getType().getSimpleName() + ", ";
+			}
+			// remove the last comma
+			sqlStatment = sqlStatment.substring(0, sqlStatment.length() - 2);
+			sqlStatment += ");";
+			s.execute(sqlStatment);
+			System.out.println("Table " + tableName + " created.");
+		} else {
+			System.out.println("Table " + tableName + " already exists.");
+		}
+		con.close();
 	}
 
 	@Override
 	public void deleteTable(final String pPath, final String pDbName, final String tableName) {
-		//Shupei
+		// Shupei
 	}
 
 	@Override
 	public void renameTable(final String pPath, final String pDbName, final String tableName) {
-		//Xen
+		// Xen
 	}
 
 	@Override
 	public void addColumn(final String pPath, final String pDbName, final String tableName, final Column... attributs) {
-		//Jessica
+		// Jessica
 	}
 
 	@Override
-	public void deleteColumn(final String pPath, final String pDbName, final String tableName, final String... attributNames) {
-		//Shupei
+	public void deleteColumn(final String pPath, final String pDbName, final String tableName,
+			final String... attributNames) {
+		// Shupei
 	}
 
 	@Override
-	public void makeColumnPrimaryKey(final String pPath, final String pDbName, final String tableName, final String... columnNames) {
-		//Xen
+	public void makeColumnPrimaryKey(final String pPath, final String pDbName, final String tableName,
+			final String... columnNames) {
+		// Xen
 	}
 
 	@Override
-	public void dropPrimaryKey(final String pPath, final String pDbName, final String tableName, final String... columnNames) {
-		//Jessica
+	public void dropPrimaryKey(final String pPath, final String pDbName, final String tableName,
+			final String... columnNames) {
+		// Jessica
 	}
 
 	@Override
-	public void makeColumnAutoIncrement(final String pPath, final String pDbName, final String tableName, final String columnNames) {
-		//Shupei
+	public void makeColumnAutoIncrement(final String pPath, final String pDbName, final String tableName,
+			final String columnNames) {
+		// Shupei
 	}
 
 	@Override
-	public void makeColumnUnique(final String pPath, final String pDbName, final String tableName, final String columnNames) {
-		//Xen
+	public void makeColumnUnique(final String pPath, final String pDbName, final String tableName,
+			final String columnNames) {
+		// Xen
 	}
 
 	@Override
-	public void makeColumnNotNull(final String pPath, final String pDbName, final String tableName, final String columnNames) {
-		//Rami
+	public void makeColumnNotNull(final String pPath, final String pDbName, final String tableName,
+			final String columnNames) {
+		// Rami
 	}
 
 }
