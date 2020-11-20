889653c3bfaab7314adcda47c08ceac29184c3a2
Burak Özdemir <73110215+BurakOezdemir@users.noreply.github.com>
2020-11-20 14:11:58 +0100

    unfinished stuff

--- de.tu_bs.cs.isf.e4cf.compare.data_structures_editor/src/de/tu_bs/cs/isf/e4cf/compare/data_structures_editor/TreeViewController.java
+++ de.tu_bs.cs.isf.e4cf.compare.data_structures_editor/src/de/tu_bs/cs/isf/e4cf/compare/data_structures_editor/TreeViewController.java
@@ -1,5 +1,6 @@
 package de.tu_bs.cs.isf.e4cf.compare.data_structures_editor;
 
+import java.io.File;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
@@ -18,47 +19,59 @@ import javafx.stage.StageStyle;
 import de.tu_bs.cs.isf.e4cf.core.util.ServiceContainer;
 import java.lang.reflect.Method;
 import java.lang.reflect.*;
+import javax.swing.JFileChooser;
 
 public class TreeViewController {
-		private ServiceContainer services;
+
+	private ServiceContainer services;
+
+	@FXML
+	private MenuItem properties;
+
+	@FXML
+	private Button search;
+
+	@FXML
+	private Label testLabel;
+
+	@FXML
+	void openProperties(ActionEvent event) throws IOException {
+		/**
+		 * Parent root;
+		 * 
+		 * Stage stage = new Stage(); stage.initModality(Modality.APPLICATION_MODAL);
+		 * stage.initStyle(StageStyle.UNDECORATED); stage.setTitle("Properties");
+		 * stage.show(); System.out.println();
+		 */
+		services = DataStructureEditorController.getServices();
+		services.partService.showPart("de.tu_bs.cs.isf.e4cf.compare.data_structures_editor.part.properties_view");
+		listingMethods(convertFileIntoClass());
+	}
+
+	public void listingMethods(Class s) {
+		Class c = s;
+		Method[] methods = c.getDeclaredMethods();
+		for (int i = 0; i < methods.length; i++) {
+			System.out.println("The method is: " + methods[i].toString());
+		}
+		// return methods;
+	}
+
+	public File chooseFile() {
+		File filePath = null;
+		JFileChooser fileChooser = new JFileChooser();
+		int choosingFile = fileChooser.showOpenDialog(null);
+		if (choosingFile == JFileChooser.APPROVE_OPTION) {
+			filePath = fileChooser.getSelectedFile();
+			System.out.println(fileChooser.getSelectedFile().getName());
+			System.out.println("kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk" +filePath);
+		}
+		return filePath;
+	}
+	public Class convertFileIntoClass() throws IOException {
 		
-
-	 	@FXML
-	    private MenuItem properties;
-
-	    @FXML
-	    private Button search;
-
-	    @FXML
-	    private Label testLabel;
-	    
-		
-	    @FXML
-	    void openProperties(ActionEvent event) throws IOException {
-	    	/**
-	    	Parent root;
-	    
-		    Stage stage = new Stage();
-		    stage.initModality(Modality.APPLICATION_MODAL);
-		    stage.initStyle(StageStyle.UNDECORATED);
-		    stage.setTitle("Properties");
-		    stage.show(); 
-		    System.out.println();
-		    */
-	    	services = DataStructureEditorController.getServices();
-	    	services.partService.showPart("de.tu_bs.cs.isf.e4cf.compare.data_structures_editor.part.properties_view");
-	    	listingMethods();
-	    	
-	    	
-	    }
-	    
-	    public void listingMethods() {
-	        Class c = TreeView.class;
-	        Method[] methods = c.getDeclaredMethods();
-	        for(int i = 0; i < methods.length; i++) {
-	            System.out.println("The method is: " + methods[i].toString());
-	         }
-			//return methods;
-	    }	
- }
-
+		File c = chooseFile();
+		Class o = c.getClass();
+		return o;
+	}
+}
