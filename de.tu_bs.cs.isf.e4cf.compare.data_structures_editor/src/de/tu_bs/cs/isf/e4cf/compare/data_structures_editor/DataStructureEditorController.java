0f3fe4659911005762e2a7517a4c41765934956b
unknown <n.baerthel@tu-braunschweig.de>
2020-11-24 15:01:34 +0100

    Added hirarchy for testing

--- de.tu_bs.cs.isf.e4cf.compare.data_structures_editor/src/de/tu_bs/cs/isf/e4cf/compare/data_structures_editor/DataStructureEditorController.java
+++ de.tu_bs.cs.isf.e4cf.compare.data_structures_editor/src/de/tu_bs/cs/isf/e4cf/compare/data_structures_editor/DataStructureEditorController.java
@@ -11,7 +11,7 @@ import org.eclipse.e4.ui.di.Persist;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
-import de.tu_bs.cs.isf.e4cf.compare.data_structures_editor.view.TreeView;
+import de.tu_bs.cs.isf.e4cf.compare.data_structures_editor.view.VisualizeTreeView;
 //import de.tu_bs.cs.isf.e4cf.compare.data_structures.editor.src.view.TreeViewController;
 import de.tu_bs.cs.isf.e4cf.core.gui.java_fx.util.FXMLLoader;
 import de.tu_bs.cs.isf.e4cf.core.util.ServiceContainer;
@@ -19,28 +19,24 @@ import javafx.embed.swt.FXCanvas;
 import javafx.scene.Scene;
 
 public class DataStructureEditorController {
-	
-	public static ServiceContainer services;
 
-	private static final String TREE_VIEW_FXML = "ui/view/TreeView.fxml";
+	private static final String TREE_VIEW_FXML = "ui/view/VisualizeTreeView.fxml";
 	public static final String COMPARATOR_VIEW_CSS_LOCATION = "css/comparator_view.css";
 	public static final String BUNDLE_NAME = "de.tu_bs.cs.isf.e4cf.compare.data_structures_editor";
 	
 	@Inject
 	public DataStructureEditorController(Composite parent, ServiceContainer services, IEclipseContext context) {
-		this.services = services;
 		postConstruct(parent, services, context);
 	}
 	@PostConstruct
 	public void postConstruct(Composite parent, ServiceContainer services, IEclipseContext context) {
-		System.out.println("Ich funktioniere.");
+		System.out.println("Ich funktioniere TreeView.");
 		FXCanvas canvans = new FXCanvas(parent, SWT.None);
-		FXMLLoader<TreeView> loader = new FXMLLoader<TreeView>(context, BUNDLE_NAME, TREE_VIEW_FXML);
-		
+		FXMLLoader<VisualizeTreeView> loader = new FXMLLoader<VisualizeTreeView>(context, BUNDLE_NAME, TREE_VIEW_FXML);
+
 		Scene scene = new Scene(loader.getNode());
 //        scene.getStylesheets().add(COMPARATOR_VIEW_CSS_LOCATION);
 		canvans.setScene(scene);
-		
 
 	}
 
@@ -57,10 +53,6 @@ public class DataStructureEditorController {
 	@Persist
 	public void save() {
 
-	}
-	
-	public static ServiceContainer getServices() {
-		return services;
 	}
 
 }
--- de.tu_bs.cs.isf.e4cf.compare.data_structures_editor/src/de/tu_bs/cs/isf/e4cf/compare/data_structures_editor/TreeViewController.java
+++ de.tu_bs.cs.isf.e4cf.compare.data_structures_editor/src/de/tu_bs/cs/isf/e4cf/compare/data_structures_editor/TreeViewController.java
@@ -1,10 +1,14 @@
 package de.tu_bs.cs.isf.e4cf.compare.data_structures_editor;
 
+import java.io.File;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import de.tu_bs.cs.isf.e4cf.compare.data_structures_editor.view.TreeView;
+import de.tu_bs.cs.isf.e4cf.compare.data_structures.impl.NodeImpl;
+import de.tu_bs.cs.isf.e4cf.compare.data_structures.impl.TreeImpl;
+
+import de.tu_bs.cs.isf.e4cf.compare.data_structures_editor.view.VisualizeTreeView;
 import de.tu_bs.cs.isf.e4cf.core.gui.java_fx.util.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,6 +16,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -19,46 +28,63 @@ import de.tu_bs.cs.isf.e4cf.core.util.ServiceContainer;
 import java.lang.reflect.Method;
 import java.lang.reflect.*;
 
+import javax.inject.Inject;
+
 public class TreeViewController {
-		private ServiceContainer services;
+	
+
+	@Inject
+	private ServiceContainer services;
+
+	@FXML
+	private MenuItem properties;
+	
+	@FXML
+    private VBox background;
+
+	@FXML
+	private Button search;
+
+	@FXML
+	private Label testLabel;
+	
+	@FXML 
+	private TreeView<?> hirarchy;
+
+	@FXML
+	void openProperties(ActionEvent event) throws IOException {
+		/**
+		 * Parent root;
+		 * 
+		 * Stage stage = new Stage(); stage.initModality(Modality.APPLICATION_MODAL);
+		 * stage.initStyle(StageStyle.UNDECORATED); stage.setTitle("Properties");
+		 * stage.show(); System.out.println();
+		 */
+		services.partService.showPart("de.tu_bs.cs.isf.e4cf.compare.data_structures_editor.part.properties_view");		
 		
-
-	 	@FXML
-	    private MenuItem properties;
-
-	    @FXML
-	    private Button search;
-
-	    @FXML
-	    private Label testLabel;
-	    
+	}
+	@FXML
+	void createTree(ActionEvent event) throws IOException {
 		
-	    @FXML
-	    void openProperties(ActionEvent event) throws IOException {
-	    	/**
-	    	Parent root;
-	    
-		    Stage stage = new Stage();
-		    stage.initModality(Modality.APPLICATION_MODAL);
-		    stage.initStyle(StageStyle.UNDECORATED);
-		    stage.setTitle("Properties");
-		    stage.show(); 
-		    System.out.println();
-		    */
-	    	services = DataStructureEditorController.getServices();
-	    	services.partService.showPart("de.tu_bs.cs.isf.e4cf.compare.data_structures_editor.part.properties_view");
-	    	listingMethods();
-	    	
-	    	
-	    }
-	    
-	    public void listingMethods() {
-	        Class c = TreeView.class;
-	        Method[] methods = c.getDeclaredMethods();
-	        for(int i = 0; i < methods.length; i++) {
-	            System.out.println("The method is: " + methods[i].toString());
-	         }
-			//return methods;
-	    }	
- }
+		TreeItem<String> rootItem = new TreeItem<String> ("class");
+		rootItem.setExpanded(true);
+		 for (int i = 1; i < 6; i++) {
+	            TreeItem<String> item = new TreeItem<String> ("Message" + i);            
+	            rootItem.getChildren().add(item);
+	        }    
+		
+		hirarchy = new TreeView<String> (rootItem);
+		background.getChildren().add(hirarchy);
+		
+		
+		System.out.println(rootItem.getValue());
+		System.out.println(background.getHeight());
+		System.out.println(hirarchy.getHeight());
+//		NodeImpl node = new NodeImpl("class");
+//		TreeImpl tree2 = new TreeImpl("testTree", node);
+//		System.out.println(node.toString());
+//		hirarchy = new TreeView<?>;
+	}
 
+	
+}
