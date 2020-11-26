b4e91e5d54ecd5971c6f5c9dba21587c9bc455ab
Serkan Acar <s.acar@tu-bs.de>
2020-11-26 09:50:11 +0100

    Add my method parts

--- de.tu_bs.cs.isf.e4cf.compare.data_structures/src/de/tu_bs/cs/isf/e4cf/compare/data_structures/io/reader/Visitor3.java
+++ de.tu_bs.cs.isf.e4cf.compare.data_structures/src/de/tu_bs/cs/isf/e4cf/compare/data_structures/io/reader/Visitor3.java
@@ -6,14 +6,31 @@ import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.PackageDeclaration;
 import com.github.javaparser.ast.body.AnnotationDeclaration;
 import com.github.javaparser.ast.body.AnnotationMemberDeclaration;
+import com.github.javaparser.ast.body.BodyDeclaration;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.TypeDeclaration;
 import com.github.javaparser.ast.expr.ArrayAccessExpr;
+import com.github.javaparser.ast.expr.ArrayInitializerExpr;
 import com.github.javaparser.ast.expr.ArrayCreationExpr;
+import com.github.javaparser.ast.ArrayCreationLevel;
+import com.github.javaparser.ast.type.ArrayType;
+import com.github.javaparser.ast.stmt.AssertStmt;
+import com.github.javaparser.ast.expr.AssignExpr;
 import com.github.javaparser.ast.expr.SimpleName;
+import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.expr.BinaryExpr;
+import com.github.javaparser.ast.expr.BooleanLiteralExpr;
+import com.github.javaparser.ast.stmt.*;
+import com.github.javaparser.ast.expr.CastExpr;
+import com.github.javaparser.ast.stmt.CatchClause;
+import com.github.javaparser.ast.expr.CharLiteralExpr;
+import com.github.javaparser.ast.expr.ClassExpr;
+import com.github.javaparser.ast.expr.ConditionalExpr;
 
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.impl.NodeImpl;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.Node;
@@ -26,23 +43,129 @@ public class Visitor3 extends VoidVisitorAdapter<Node>{
 		Node child = new NodeImpl("CompilationUnit");
 		n.addChild(child);
 		super.visit(cu, child);
+		if (cu.getTypes() != null) {
+			for (TypeDeclaration<?> tD : cu.getTypes()) {
+				n.addAttribute("Type Declaration", tD.toString());
+			}
+		}
 	}
 	
 	@Override
-	public void visit(MethodDeclaration md, Node n) {
-		Node child = new NodeImpl(md.getNameAsString());
+	public void visit(AnnotationDeclaration u, Node n) {
+		n.getParent().addAttribute("AnnotationDeclaration", u.toString());
+		if (u.getMembers() != null) {
+			for (BodyDeclaration<?> member : u.getMembers()) {
+				n.getParent().addAttribute("Member", member.toString());
+			}
+		}
+	}
+	
+	@Override
+	public void visit(AnnotationMemberDeclaration u, Node n) {
+		Node child =  new NodeImpl(u.getNameAsString().toString());
 		n.addChild(child);
-		super.visit(md, child);
+		super.visit(u, child);
+		n.addAttribute("Membertyp", u.getType().toString());
 	}
 	
 	@Override
-	public void visit(Modifier mod, Node n) {
-		n.getParent().addAttribute("Modifier", mod.toString());
+	public void visit(ArrayAccessExpr u, Node n) {
+		Node child = new NodeImpl(u.getName().toString());
+		n.addChild(child);
+		super.visit(u, child);
+		n.addAttribute("Expression", u.toString());
 	}
 	
 	@Override
-	public void visit(SimpleName sn, Node n) {
-		n.getParent().addAttribute("SimpleName", sn.toString());
+	public void visit(ArrayCreationExpr u, Node n) {
+		n.addAttribute("Array Creation Expression Element Type", u.getElementType().toString());
+		for (ArrayCreationLevel level : u.getLevels()) {
+			n.getParent().addAttribute("Level", level.toString());
+		}
+	}
+	
+	@Override
+	public void visit(ArrayCreationLevel u, Node n) {
+		n.getParent().addAttribute("ArrayCreationLevel", u.toString());
+	}
+	
+	@Override
+	public void visit(ArrayInitializerExpr u, Node n) {
+		if (u.getValues() != null) {
+			for (Expression expr : u.getValues()) {
+				n.addAttribute("Expression", expr.toString());
+			}
+		}
+	}
+	
+	@Override
+	public void visit(ArrayType u, Node n) {
+		n.getParent().addAttribute("Array Type", u.getComponentType().toString());
+	}
+	
+	@Override
+	public void visit(AssertStmt u, Node n) {
+		n.getParent().addAttribute("AssertStmt", u.getMessage().toString());
+	}
+	
+	@Override
+	public void visit(AssignExpr u, Node n) {
+		n.getParent().addAttribute("AssignExpr", u.getValue().toString());
+	}
+	
+	@Override
+	public void visit(BinaryExpr u, Node n) {
+		Node child = new NodeImpl(u.toString());
+		n.addChild(child);
+		super.visit(u, child);
+		n.addAttribute("Expression Left", u.getLeft().toString());
+		n.addAttribute("Expression Right", u.getRight().toString());
+		super.visit(u, child);
+	}
+	
+	@Override
+	public void visit(BlockStmt u, Node n) {
+		Node child = new NodeImpl(u.toString());
+		n.addChild(child);
+		super.visit(u, child);
+		n.addAttribute("Block Statement", u.toString());
+		if (u.getStatements() != null) {
+			for (Statement s : u.getStatements()) {
+				n.addAttribute("Statement", s.toString());
+			}
+		}
+	}
+	
+	@Override
+	public void visit(BooleanLiteralExpr u, Node n) {
+		n.addAttribute("Boolean Literal Expression", u.toString());
+	}
+	
+	@Override
+	public void visit(BreakStmt u, Node n) {
+		n.addAttribute("Break Statement", u.toString());
+	}
+	
+	@Override
+	public void visit(CastExpr u, Node n) {
+		n.addAttribute("Typ", u.getType().toString());
+		n.addAttribute("Expression", u.getExpression().toString());
+	}
+	
+	@Override
+	public void visit(CatchClause u, Node n) {
+		n.addAttribute("Parameter", u.getParameter().toString());
+		n.addAttribute("Body", u.getBody().toString());
+	}
+	
+	@Override
+	public void visit(CharLiteralExpr u, Node n) {
+		n.addAttribute("Char Literal Expression", u.toString());	
+	}
+	
+	@Override
+	public void visit(ClassExpr u, Node n) {
+		n.addAttribute("Class Expression Type", u.getType().toString());	
 	}
 	
 	@Override
@@ -60,28 +183,32 @@ public class Visitor3 extends VoidVisitorAdapter<Node>{
 			n.addAttribute("Implemented" + implementedTypeCtr, cit.getNameAsString());
 		}
 	}
-
-	@Override // check it
-	public void visit(AnnotationDeclaration u, Node n) {
-		n.getParent().addAttribute("AnnotationDeclaration", u.toString());
-	}
 	
-	@Override // check it
-	public void visit(AnnotationMemberDeclaration u, Node n) {
-		n.getParent().addAttribute("AnnotationMemberDeclaration", u.toString());
-	}
-	
-	@Override // needed?
-	public void visit(ArrayAccessExpr u, Node n) {
-		Node child = new NodeImpl(u.getName().toString());
+	@Override
+	public void visit(ConditionalExpr u, Node n) {
+		Node child = new NodeImpl(u.toString());
 		n.addChild(child);
 		super.visit(u, child);
-		n.addAttribute("Expression", u.toString());
+		n.addAttribute("Condition", u.getCondition().toString());
+		n.addAttribute("Then", u.getThenExpr().toString());
+		n.addAttribute("Else", u.getElseExpr().toString());
 	}
 	
 	@Override
-	public void visit(ArrayCreationExpr u, Node n) {
-		//
+	public void visit(MethodDeclaration md, Node n) {
+		Node child = new NodeImpl(md.getNameAsString());
+		n.addChild(child);
+		super.visit(md, child);
+	}
+	
+	@Override
+	public void visit(Modifier mod, Node n) {
+		n.getParent().addAttribute("Modifier", mod.toString());
+	}
+	
+	@Override
+	public void visit(SimpleName sn, Node n) {
+		n.getParent().addAttribute("SimpleName", sn.toString());
 	}
 	
 	@Override
