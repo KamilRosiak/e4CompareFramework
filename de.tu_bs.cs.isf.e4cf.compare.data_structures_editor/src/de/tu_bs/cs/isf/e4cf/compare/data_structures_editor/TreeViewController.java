20e22454cb96d9d7d6fd320b45bf68b36b7b2f12
Burak Özdemir <73110215+BurakOezdemir@users.noreply.github.com>
2020-12-18 13:33:31 +0100

    added delete and rename function to context menu

--- de.tu_bs.cs.isf.e4cf.compare.data_structures_editor/src/de/tu_bs/cs/isf/e4cf/compare/data_structures_editor/TreeViewController.java
+++ de.tu_bs.cs.isf.e4cf.compare.data_structures_editor/src/de/tu_bs/cs/isf/e4cf/compare/data_structures_editor/TreeViewController.java
@@ -161,4 +161,18 @@ public class TreeViewController {
     void incrementSearchCounter() {
     	searchCounter++;
     }
+	@FXML
+	void deleteNode() {
+		TreeItem<NodeUsage> deletingNode = treeView.getSelectionModel().getSelectedItem();
+		treeView.getSelectionModel().getSelectedItem().setValue(null);
+		treeView.getSelectionModel().getSelectedItem().setGraphic(null);
+		deletingNode.getParent().getChildren().remove(deletingNode);
+
+	}
+
+	@FXML
+	void renameNode() {
+		TreeItem<NodeUsage> renamingNode = treeView.getSelectionModel().getSelectedItem();
+
+	}
 }
