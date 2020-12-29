4573e7436d7f5468256f589b8d036855f60e9aa6
Xue Ren <rxedelweiss@gmail.com>
2020-12-29 23:27:03 +0800

    method getRowCounts

--- de.tu_bs.cs.isf.e4cf.core/src/de/tu_bs/cs/isf/e4cf/core/db/DataUtilities.java
+++ de.tu_bs.cs.isf.e4cf.core/src/de/tu_bs/cs/isf/e4cf/core/db/DataUtilities.java
@@ -1,5 +1,10 @@
 package de.tu_bs.cs.isf.e4cf.core.db;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import de.tu_bs.cs.isf.e4cf.core.db.model.AddCondition;
 import de.tu_bs.cs.isf.e4cf.core.db.model.Column;
 import de.tu_bs.cs.isf.e4cf.core.db.model.Sorting;
@@ -82,4 +87,29 @@ public class DataUtilities {
 		orderSql += " " + sort.getOrderType() + ";";
 		return orderSql;
 	}
+	
+	 /**
+		 * Method to get the number of rows.
+		 * 
+		 * @param pPath      String the path of the database
+		 * @param pDbName    String the name of the database
+		 * @param tableName  String name of the table
+		 * @return int
+		 * @throws SQLException
+		 */
+	   
+	   public int getRowsCount(final String pPath, final String pDbName, final String pTableName) 
+		  throws SQLException {
+				final Connection con = DatabaseFactory.getInstance().getDatabase(pPath, pDbName);
+				Statement stm =  con.createStatement();
+				ResultSet rs = stm.executeQuery("SELECT * FROM " + pTableName);
+				int rowcounts = 0;
+				while (rs.next()) {
+					rowcounts++ ;
+				}
+				con.close();
+				System.out.println(rowcounts);
+				return rowcounts;
+			}
+	   
 }
