2ada9c5dd79c646f7d3c16e8dfd2550267597b99
Karl Störmer <karlstoermer97@gmail.com>
2021-01-08 23:26:13 +0100

    Use Paths.get() for creation of new folders
    Task #25

--- de.tu_bs.cs.isf.e4cf.parts.project_explorer/src/de/tu_bs/cs/isf/e4cf/parts/project_explorer/handlers/NewFolderHandler.java
+++ de.tu_bs.cs.isf.e4cf.parts.project_explorer/src/de/tu_bs/cs/isf/e4cf/parts/project_explorer/handlers/NewFolderHandler.java
@@ -2,6 +2,7 @@
 package de.tu_bs.cs.isf.e4cf.parts.project_explorer.handlers;
 
 import java.io.File;
+import java.nio.file.Paths;
 
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.CanExecute;
@@ -14,16 +15,16 @@ import de.tu_bs.cs.isf.e4cf.core.util.services.RCPSelectionService;
 
 public class NewFolderHandler implements IHandler {
 
-	private static final String FOLDER_PLACEHOLDER = "/New Folder";
+	private static final String FOLDER_PLACEHOLDER = "New Folder";
 
 	@Execute
 	public void execute(IEclipseContext context, ServiceContainer services, Shell shell) {
 		FileTreeElement element = services.rcpSelectionService.getCurrentSelectionFromExplorer();
 		String stringPath = element.getAbsolutePath();
-		File folderToCreate = new File(stringPath + FOLDER_PLACEHOLDER);
+		File folderToCreate = new File(Paths.get(stringPath, FOLDER_PLACEHOLDER).toAbsolutePath().toString());
 		int folderNameExistsCounter = 2;
 		while (folderToCreate.exists()) {
-			folderToCreate = new File(stringPath + FOLDER_PLACEHOLDER + " (" + folderNameExistsCounter + ")");
+			folderToCreate = new File(Paths.get(stringPath, FOLDER_PLACEHOLDER + " (" + folderNameExistsCounter + ")").toAbsolutePath().toString());
 			folderNameExistsCounter++;
 		}
 		folderToCreate.mkdir();
