f57b5f85779eb75ff3f48eb250b5e16558a7fbd4
Lars Mühlenstädt <muehlenstaedt.lars@gmail.com>
2021-01-14 15:52:39 +0100

    move gson adapter

--- de.tu_bs.cs.isf.e4cf.core.import_export/src/de/tu_bs/cs/isf/e4cf/core/import_export/services/gson/adapter/TreeInstanceCreator.java
+++ de.tu_bs.cs.isf.e4cf.core.import_export/src/de/tu_bs/cs/isf/e4cf/core/import_export/services/gson/adapter/TreeInstanceCreator.java
@@ -1,4 +1,4 @@
-package de.tu_bs.cs.isf.e4cf.core.import_export.services.adapter;
+package de.tu_bs.cs.isf.e4cf.core.import_export.services.gson.adapter;
 
 import com.google.gson.InstanceCreator;
 
@@ -7,6 +7,12 @@ import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.Tree;
 
 import java.lang.reflect.Type;
 
+/**
+ * This is needed to allow the creation of treeImpl while importing data.
+ * The treeImpl class does not have a constructor without a parameter.
+ * 
+ * @author Team 6
+ */
 public class TreeInstanceCreator implements InstanceCreator<Tree> {
 	@Override
 	public Tree createInstance(Type type) {
