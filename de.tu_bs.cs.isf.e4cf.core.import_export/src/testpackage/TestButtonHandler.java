5fef24e285fe94e2b02e8624e1a16fef9458efaf
Richard Neumann <mail@richard-neumann.de>
2021-01-18 13:50:54 +0100

    Refactored indentation.

--- de.tu_bs.cs.isf.e4cf.core.import_export/src/testpackage/TestButtonHandler.java
+++ de.tu_bs.cs.isf.e4cf.core.import_export/src/testpackage/TestButtonHandler.java
@@ -10,15 +10,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.enums.VariabilityClass;
-//import de.tu_bs.cs.isf.e4cf.compare.data_structures.enums.VariabilityClass;
-//import de.tu_bs.cs.isf.e4cf.compare.data_structures.impl.AttributeImpl;
-//import de.tu_bs.cs.isf.e4cf.compare.data_structures.impl.NodeImpl;
-//import de.tu_bs.cs.isf.e4cf.compare.data_structures.impl.TreeImpl;
-//import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.AbstractAttribute;
-//import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.AbstractNode;
-//import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.AbstractTree;
-//import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.Attribute;
-//import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.Node;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.impl.AttributeImpl;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.impl.NodeImpl;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.impl.TreeImpl;
@@ -31,61 +22,58 @@ import util.TreeInstanceCreator;
 
 
 
-public class TestButtonHandler {	
-	
+public class TestButtonHandler {
 	@Execute
 	public void execute() {
 		System.out.println("ayyyy");
-		 GsonBuilder gsonBuilder = new GsonBuilder();
-	        gsonBuilder.registerTypeAdapter(Tree.class, new TreeInstanceCreator());
+		GsonBuilder gsonBuilder = new GsonBuilder();
+		gsonBuilder.registerTypeAdapter(Tree.class, new TreeInstanceCreator());
 
 
-	        Set<String> stringSet = new HashSet<>();
-	        stringSet.add("value1");
-	        stringSet.add("value2");
+		Set<String> stringSet = new HashSet<>();
+		stringSet.add("value1");
+		stringSet.add("value2");
 
 
-	        List<Attribute> testAttributes = new LinkedList<>();
-	        testAttributes.add(new AttributeImpl("Key1","Value"));
+		List<Attribute> testAttributes = new LinkedList<>();
+		testAttributes.add(new AttributeImpl("Key1","Value"));
 
-	        NodeImpl root = new NodeImpl("Root");
-	        root.setAttributes(testAttributes);
-	        root.setVariabilityClass(VariabilityClass.MANDATORY);
-	        root.setChildren(new LinkedList<>());
+		NodeImpl root = new NodeImpl("Root");
+		root.setAttributes(testAttributes);
+		root.setVariabilityClass(VariabilityClass.MANDATORY);
+		root.setChildren(new LinkedList<>());
 
-	        TreeImpl tree = new TreeImpl("testTree");
-	        tree.setArtifactType("type");
-	        tree.setRoot(root);
+		TreeImpl tree = new TreeImpl("testTree");
+		tree.setArtifactType("type");
+		tree.setRoot(root);
 
-	        System.out.println(createJSON(tree, gsonBuilder));
+		System.out.println(createJSON(tree, gsonBuilder));
 
-	        TreeImpl treeResult = (TreeImpl) readJSON(createJSON(tree, gsonBuilder), gsonBuilder);
+		TreeImpl treeResult = (TreeImpl) readJSON(createJSON(tree, gsonBuilder), gsonBuilder);
 
-	        reconstructTree(treeResult.getRoot());
+		reconstructTree(treeResult.getRoot());
 
-	        System.out.println(treeResult.toString());
+		System.out.println(treeResult.toString());
+	}
 
+	private static void reconstructTree(Node node) {
+		if (node == null || node.getChildren() == null) {
+			return;
+		}
 
-	    }
+		for (Node children : node.getChildren()) {
+			children.setParent(node);
+			reconstructTree(children);
+		}
+	}
 
-	    private static void reconstructTree(Node node) {
-	        if (node == null || node.getChildren() == null) return;
-	        for (Node children : node.getChildren()) {
-	            children.setParent(node);
-	            reconstructTree(children);
-	        }
-	    }
+	private static String createJSON(Object object, GsonBuilder builder) {
+		Gson gson = builder.create();
+		return gson.toJson(object);
+	}
 
-	    private static String createJSON(Object object, GsonBuilder builder) {
-	        Gson gson = builder.create();
-	        return gson.toJson(object);
-	    }
-
-	    private static Object readJSON(String jsonString, GsonBuilder builder) {
-	        Gson gson = builder.create();
-	        return gson.fromJson(jsonString, Tree.class);
-	    }
-	
-	
-		
+	private static Object readJSON(String jsonString, GsonBuilder builder) {
+		Gson gson = builder.create();
+		return gson.fromJson(jsonString, Tree.class);
+	}
 }
--- de.tu_bs.cs.isf.e4cf.core.import_export/src/util/TreeInstanceCreator.java
+++ de.tu_bs.cs.isf.e4cf.core.import_export/src/util/TreeInstanceCreator.java
@@ -8,9 +8,8 @@ import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.Tree;
 import java.lang.reflect.Type;
 
 public class TreeInstanceCreator implements InstanceCreator<Tree> {
-    @Override
-    public Tree createInstance(Type type) {
-        return new TreeImpl("");
-    }
+        @Override
+        public Tree createInstance(Type type) {
+                return new TreeImpl("");
+        }
 }
-
