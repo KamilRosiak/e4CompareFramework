8908dba72d9b52e67b6bb393f8a4e915101badf3
erwin97 <erwinwijaya@gmx.de>
2021-01-21 14:01:05 +0100

    add auto inden for XML and auto close bracket for java

--- de.tu_bs.cs.isf.e4cf.text_editor/src/de/tu_bs/cs/isf/e4cf/text_editor/indentation/JavaIndentation.java
+++ de.tu_bs.cs.isf.e4cf.text_editor/src/de/tu_bs/cs/isf/e4cf/text_editor/indentation/JavaIndentation.java
@@ -5,9 +5,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.model.Paragraph;
 
 import javafx.application.Platform;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
 
 /**
@@ -18,7 +20,7 @@ import javafx.scene.input.KeyEvent;
  */
 public class JavaIndentation {
     private String whiteSpaceRegex = "^\\s+";
-    private String braceRegex = "[{(]\\s*$";
+    private String braceRegex = "[{(\\[]\\s*$";
 
     private CodeArea codeArea;
 
@@ -37,22 +39,34 @@ public class JavaIndentation {
         Pattern whiteSpace = Pattern.compile(whiteSpaceRegex);
         Pattern brace = Pattern.compile(braceRegex);
         codeArea.addEventFilter(KeyEvent.KEY_PRESSED, KE -> {
-            if (KE.getCode() == KeyCode.ENTER) {
-                List<String> segments = codeArea.getParagraph(codeArea.getCurrentParagraph()).getSegments();
-                Matcher whiteSpaceInFront = whiteSpace.matcher(segments.get(0));
-                Matcher newBlock = brace.matcher(segments.get(segments.size() - 1));
+            String line = codeArea.getParagraph(codeArea.getCurrentParagraph()).substring(0, codeArea.getCaretColumn());
+            if (KE.getCode() == KeyCode.ENTER) {       
+                Matcher whiteSpaceInFront = whiteSpace.matcher(line);
+                Matcher newBlock = brace.matcher(line);
                 
                 if (newBlock.find()) {
                     // indent with one tab
-                    Platform.runLater(() -> codeArea.insertText(codeArea.getCaretPosition(), "\t"));
+                	Platform.runLater(() -> codeArea.insertText(codeArea.getCaretPosition(), "\t"));           	                 
                 }
 
                 if (whiteSpaceInFront.find()) {
                     // add indentation of the previous line to the new line
+                	
                     Platform.runLater(() -> codeArea.insertText(codeArea.getCaretPosition(), whiteSpaceInFront.group()));
                 }
+            } else if (KE.getCode() == KeyCode.DIGIT7 && KE.isAltDown() && KE.isControlDown()) {
+                insertClosingBrace("}");
+            } else if (KE.getCode() == KeyCode.DIGIT8 && KE.isAltDown() && KE.isControlDown()) {
+            	insertClosingBrace("]");
+            } else if (KE.getCode() == KeyCode.DIGIT8 && KE.isShiftDown()) {
+            	insertClosingBrace(")");
             }
         });
+    }
+
+    private void insertClosingBrace(String brace) {
+        Platform.runLater(() -> codeArea.insertText(codeArea.getCaretPosition(), brace));
+        Platform.runLater(() -> codeArea.moveTo(codeArea.getCaretPosition() - 1));
     }
 
 }
