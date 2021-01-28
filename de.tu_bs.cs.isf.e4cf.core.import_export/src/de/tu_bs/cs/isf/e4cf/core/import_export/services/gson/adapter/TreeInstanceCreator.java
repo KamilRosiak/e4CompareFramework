e66c92d89e371a2251ff117815979e709fd4eada
Lars Mühlenstädt <muehlenstaedt.lars@gmail.com>
2021-01-28 15:55:39 +0100

    merge

--- de.tu_bs.cs.isf.e4cf.core.import_export/src/de/tu_bs/cs/isf/e4cf/core/import_export/services/gson/adapter/TreeInstanceCreator.java
+++ de.tu_bs.cs.isf.e4cf.core.import_export/src/de/tu_bs/cs/isf/e4cf/core/import_export/services/gson/adapter/TreeInstanceCreator.java
@@ -8,11 +8,7 @@ import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.Tree;
 import java.lang.reflect.Type;
 
 /**
-<<<<<<< HEAD:de.tu_bs.cs.isf.e4cf.core.compare.remote/src/de/tu_bs/cs/isf/e4cf/core/compare/remote/util/TreeInstanceCreator.java
  * Factory to create instances implementing the Tree interface.
-=======
- * Factory to create instaces implementing the Tree interface.
->>>>>>> conflict_fix:de.tu_bs.cs.isf.e4cf.core.import_export/src/de/tu_bs/cs/isf/e4cf/core/import_export/services/gson/adapter/TreeInstanceCreator.java
  */
 public class TreeInstanceCreator implements InstanceCreator<Tree> {
 	/**
