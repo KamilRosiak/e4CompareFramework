b4be608ca575ae98807670d4d5a9fba7a475092b
Lars Mühlenstädt <muehlenstaedt.lars@gmail.com>
2021-02-04 16:08:20 +0100

    add javadocs

--- de.tu_bs.cs.isf.e4cf.core.compare.remote/src/de/tu_bs/e4cf/core/compare/remote/contribution/preferences/RemoteComparisonPreferencePage.java
+++ de.tu_bs.cs.isf.e4cf.core.compare.remote/src/de/tu_bs/e4cf/core/compare/remote/contribution/preferences/RemoteComparisonPreferencePage.java
@@ -4,16 +4,26 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
+import de.tu_bs.cs.isf.e4cf.core.compare.remote.config.RemoteComparisonStringTable;
 import de.tu_bs.cs.isf.e4cf.core.preferences.interfaces.IPreferencePage;
 import de.tu_bs.cs.isf.e4cf.core.preferences.util.PreferencesUtil;
+import de.tu_bs.cs.isf.e4cf.core.preferences.util.key_value.KeyValueNode;
 import de.tu_bs.cs.isf.e4cf.core.util.ServiceContainer;
-import de.tu_bs.e4cf.core.compare.remote.contribution.preferences.contribution.RemoteComparisonConfigurationContribution;
 
 public class RemoteComparisonPreferencePage implements IPreferencePage {
-	RemoteComparisonConfigurationContribution contribution;
-
+	private KeyValueNode ipConfiguration;
+	private KeyValueNode portConfiguration;
+	private Text textFieldIp;
+	private Text textFieldPort;
+	
 	@Override
 	public void createPage(CTabFolder parent, ServiceContainer services) {
 		CTabItem tab = new CTabItem(parent, SWT.NONE);
@@ -22,15 +32,52 @@ public class RemoteComparisonPreferencePage implements IPreferencePage {
 		Composite page = new Composite(parent, SWT.None);
 		page.setLayout(new FillLayout(SWT.VERTICAL));
 
-		contribution = new RemoteComparisonConfigurationContribution(page);
+		createControl(page);
 
 		tab.setControl(page);
 	}
 
+	private void createControl(Composite parent) {
+		Group group = new Group(parent, SWT.None);
+		group.setText("Connection");
+		group.setLayout(new GridLayout(3, false));
+
+		ipConfiguration = PreferencesUtil.getValueWithDefault(RemoteComparisonStringTable.BUNDLE_NAME,
+				RemoteComparisonStringTable.IP_KEY, RemoteComparisonStringTable.DEFAULT_IP);
+		portConfiguration = PreferencesUtil.getValueWithDefault(RemoteComparisonStringTable.BUNDLE_NAME,
+				RemoteComparisonStringTable.PORT_KEY, RemoteComparisonStringTable.DEFAULT_PORT);
+
+		textFieldIp = createTextField(group, "Address", ipConfiguration);
+		textFieldPort = createTextField(group, "Port", portConfiguration);
+	}
+
+	private Text createTextField(Group group, String label, KeyValueNode keyValueNode) {
+
+		Label uriLabel = new Label(group, SWT.NONE);
+		uriLabel.setText(label);
+		uriLabel.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false));
+
+		Text textField = new Text(group, SWT.SINGLE | SWT.BORDER);
+		textField.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, true));
+		textField.setBackground(Display.getDefault().getSystemColor(SWT.COLOR_WHITE));
+		textField.setText(keyValueNode.getStringValue());
+		return textField;
+	}
+	
+	public KeyValueNode getIpValue() {
+		ipConfiguration.setValue(textFieldIp.getText());
+		return ipConfiguration;
+	}
+
+	public KeyValueNode getPortValue() {
+		portConfiguration.setValue(textFieldPort.getText());
+		return portConfiguration;
+	}
+	
 	@Override
 	public void store() {
-		PreferencesUtil.storeKeyValueNode(contribution.getIpValue());
-		PreferencesUtil.storeKeyValueNode(contribution.getPortValue());
+		PreferencesUtil.storeKeyValueNode(getIpValue());
+		PreferencesUtil.storeKeyValueNode(getPortValue());
 	}
 
 }
