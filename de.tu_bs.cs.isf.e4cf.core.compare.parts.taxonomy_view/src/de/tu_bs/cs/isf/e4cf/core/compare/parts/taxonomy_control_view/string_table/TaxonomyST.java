dbf499ee6e7c549a1c996e744ac0dc65d46d47ec
developer-olan <olantim@aol.com>
2021-09-26 10:36:49 +0200

    Updated taxonomy event strings ins tring table

--- de.tu_bs.cs.isf.e4cf.core.compare.parts.taxonomy_view/src/de/tu_bs/cs/isf/e4cf/core/compare/parts/taxonomy_control_view/string_table/TaxonomyST.java
+++ de.tu_bs.cs.isf.e4cf.core.compare.parts.taxonomy_view/src/de/tu_bs/cs/isf/e4cf/core/compare/parts/taxonomy_control_view/string_table/TaxonomyST.java
@@ -9,5 +9,6 @@ package de.tu_bs.cs.isf.e4cf.core.compare.parts.taxonomy_control_view.string_tab
  */
 public class TaxonomyST {
 	public static final String BUNDLE_NAME = "de.tu_bs.cs.isf.e4cf.core.compare.parts.taxonomy_control_view";
+	public static final String SET_TAXONOMY_SETTINGS ="SET_TAXONOMY_SETTINGS";
 	public static final String SAVE_TAXONOMY_EVENT ="SAVE_TAXONOMY_EVENT";
 }
