4f0b41b54efd7e9daaee4bfabb029889fa7dccd9
Kamil Rosiak <k.rosiak@tu-bs.de>
2021-10-22 12:04:12 +0200

    removed whitespaces

--- de.tu_bs.cs.isf.e4cf.refactoring/src/de/tu_bs/cs/isf/e4cf/refactoring/extraction/ClusterEngine.java
+++ de.tu_bs.cs.isf.e4cf.refactoring/src/de/tu_bs/cs/isf/e4cf/refactoring/extraction/ClusterEngine.java
@@ -79,7 +79,6 @@ public class ClusterEngine {
 
 			layerToClusters.put(entry.getKey(),
 					detectClusters(entry.getValue(), buildDistanceString(entry.getValue())));
-
 		}
 
 		return layerToClusters;
