f771756b2727d9fa4eaea16406cc3fdc68f5dc47
developer-olan <olantim@aol.com>
2021-11-14 17:00:13 +0100

    Added code to cater for number of characters of variants or artifacts

--- de.tu_bs.cs.isf.e4cf.compare.taxonomy/src/de/tu_bs/cs/isf/e4cf/compare/taxonomy/data_structures/ArtifactFileDetails.java
+++ de.tu_bs.cs.isf.e4cf.compare.taxonomy/src/de/tu_bs/cs/isf/e4cf/compare/taxonomy/data_structures/ArtifactFileDetails.java
@@ -8,6 +8,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 import de.tu_bs.cs.isf.e4cf.core.file_structure.FileTreeElement;
+import de.tu_bs.cs.isf.e4cf.core.util.file.FileStreamUtil;
 
 /**
  * @author developer-olan
@@ -19,9 +20,11 @@ public class ArtifactFileDetails {
 	private String artifactID;
 	private String artifactPath;
 	private int artifactLinesOfCode;
-	private int numberOfCharactersInArtifact;
+	private int numberOfSourceCodeCharactersInArtifact;
 	private boolean isDirectory;
 	private long totalByteSize = 0;
+	private int allSubFiles = 0;
+	private int allSubFolders = 0;
 
 	public ArtifactFileDetails(FileTreeElement _artifactFileTreeElement) {
 		this.isDirectory = _artifactFileTreeElement.isDirectory();
@@ -30,9 +33,10 @@ public class ArtifactFileDetails {
 		this.artifactPath = Paths.get(_artifactFileTreeElement.getAbsolutePath()).toString();
 		// Computed Class attributes
 		this.artifactLinesOfCode = this.countLinesOfCode(Paths.get(_artifactFileTreeElement.getAbsolutePath()));
-		this.numberOfCharactersInArtifact = this
+		this.numberOfSourceCodeCharactersInArtifact = this
 				.countNumberOfCharacters(Paths.get(_artifactFileTreeElement.getAbsolutePath()));
 		computeSizeInBytes(this.artifactFileTreeElement);
+		computeSubContents(this.artifactFileTreeElement);
 	}
 
 	/**
@@ -67,14 +71,11 @@ public class ArtifactFileDetails {
 	 */
 	private int countNumberOfCharacters(Path aParsedFile) {
 		int numberOfCharacters = 0;
-
 		try {
 			if (!isDirectory) {
 				List<String> codeInLines = Files.readAllLines(Paths.get(aParsedFile.toUri()), StandardCharsets.UTF_8);
-				numberOfCharacters = codeInLines.size();
-				for (String aLine : codeInLines) {
-					numberOfCharacters += aLine.length();
-				}
+				codeInLines.forEach(line -> line.replaceAll("\\s+",""));
+				numberOfCharacters = FileStreamUtil.readLineByLine(Paths.get(aParsedFile.toUri())).replaceAll("\\s+","").length();
 			} else if(isDirectory) {
 				numberOfCharacters = getNumberOfChildren();
 			}
@@ -110,9 +111,14 @@ public class ArtifactFileDetails {
 		return artifactPath;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public FileTreeElement getFileTree() {
 		return this.artifactFileTreeElement;
 	}
+	
 	/**
 	 * Returns the number of lines of code contained in a specific variant
 	 * 
@@ -123,19 +129,23 @@ public class ArtifactFileDetails {
 	}
 
 	/**
+	 * 
 	 * Returns the number of characters in a Variant
 	 * 
 	 * @return
 	 */
 	public int getVariantSize() {
 		if (artifactFileTreeElement.isDirectory()) {
+//			return (int)totalByteSize;
 			return (int)totalByteSize;
+
 		} else {
-			return numberOfCharactersInArtifact;
+			return numberOfSourceCodeCharactersInArtifact;
 		}
 	}
 
 	/**
+
 	 * Returns the number of children if artifact is a directory
 	 * @return
 	 */
@@ -148,15 +158,43 @@ public class ArtifactFileDetails {
 	}
 	
 	
+	public int getNumberOfSubFolders() {
+		computeSubContents(this.artifactFileTreeElement);
+		return this.allSubFolders;
+	}
+	
+	public int getNumberOfSubFiles() {
+		getNumberOfSubFolders();
+		return this.allSubFiles;
+	}
+	
+	public int getNumberOfCharactersInArtifact() {
+		int numberOfCharacters = 0;
+		if (!isDirectory) {
+			numberOfCharacters = numberOfSourceCodeCharactersInArtifact;
+		}
+		return numberOfCharacters;
+	}
+	
+	
 	/**
 	 * Returns the number of children if artifact is a directory
 	 * @return
 	 */
-	public void computeSizeInBytes(FileTreeElement tree) {
+	private void computeSizeInBytes(FileTreeElement tree) {
 		if (tree.isDirectory()) {
 			tree.getChildren().stream().forEach(e -> computeSizeInBytes(e));
 		} else {
 			this.totalByteSize += tree.getSize();
+		}
+	}
+	
+	public void computeSubContents(FileTreeElement tree) {
+		if (tree.isDirectory()) {
+			this.allSubFolders++;
+			tree.getChildren().stream().forEach(e -> computeSubContents(e));
+		} else {
+			this.allSubFiles++;
 		}
 	}
 	
