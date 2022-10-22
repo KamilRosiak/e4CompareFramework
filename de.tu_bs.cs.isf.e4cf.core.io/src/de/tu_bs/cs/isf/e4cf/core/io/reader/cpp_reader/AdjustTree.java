486db91a4a23d0631b9df5d9a5d3b1e4fada9619
David Bumm <uusiy@student.kit.edu>
2022-10-22 13:51:17 +0200

    useless Treenodes are now deleted

--- de.tu_bs.cs.isf.e4cf.core.io/src/de/tu_bs/cs/isf/e4cf/core/io/reader/cpp_reader/AdjustTree.java
+++ de.tu_bs.cs.isf.e4cf.core.io/src/de/tu_bs/cs/isf/e4cf/core/io/reader/cpp_reader/AdjustTree.java
@@ -3,6 +3,7 @@ package de.tu_bs.cs.isf.e4cf.core.io.reader.cpp_reader;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.impl.AttributeImpl;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.impl.NodeImpl;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.impl.StringValueImpl;
+import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.Attribute;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.Node;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.Tree;
 
@@ -12,16 +13,13 @@ import java.util.List;
 public final class AdjustTree {
 	private Node rootNode;
 
-	public AdjustTree(Node rootNode)
-	{
+	public AdjustTree(Node rootNode) {
 		this.rootNode = rootNode;
 	}
 
-
-	public Node adjustAllNodes()
-	{
+	public Node adjustAllNodes() {
 		if (rootNode == null) {
-			return null; 
+			return null;
 		}
 		Node node = new NodeImpl("JAVA");
 		node.addChild(rootNode);
@@ -36,18 +34,38 @@ public final class AdjustTree {
 		if (node == null) {
 			return;
 		}
-		if (node.getNodeType().equals("Name") || node.getNodeType().equals("type")) {
-			node.setParent(null);
+		Node parent = node.getParent();
+		String nodeType = node.getNodeType();
 
+		if (nodeType.equals("Name") || nodeType.equals("type")) {
+			int index = parent.getChildren().indexOf(node);
+			parent.getChildren().remove(index);
 			return;
 		}
-		if (node.getNodeType().equals("FieldDeclaration") //enum edge case
-				&& node.getParent().getParent().getNodeType().equals("EnumDeclaration")) {
+		if (nodeType.equals("Body") && (parent.getNodeType().equals("EnumDeclaration")
+				|| (parent.getNodeType().equals("Body")))) {
+			List<Node> children = new ArrayList<Node>(node.getChildren());
+			for (Node child : children) {
+				parent.addChild(child);
+				child.setParent(parent);
+			}
+			int index = parent.getChildren().indexOf(node);
+			parent.getChildren().remove(index);
+		}
+		if (nodeType.equals("FieldDeclaration") // enum edge case
+				&& parent.getNodeType().equals("EnumDeclaration")) {
 			node.setNodeType("EnumConstantDeclaration");
 		}
-		
-		if (node.getNumberOfChildren() > 0)
-		{
+		if (nodeType.equals("FieldDeclaration") && parent.getNodeType().equals("Argument")) {
+			List<Attribute> attrList = new ArrayList<Attribute>(node.getAttributes());
+			for (Attribute attribute : attrList) {
+				parent.addAttribute(attribute);
+			}
+			int index = parent.getChildren().indexOf(node);
+			parent.getChildren().remove(index);
+		}
+
+		if (node.getNumberOfChildren() > 0) {
 			List<Node> children = new ArrayList<Node>(node.getChildren());
 			for (Node child : children) {
 				recursiveAdjust(child);
