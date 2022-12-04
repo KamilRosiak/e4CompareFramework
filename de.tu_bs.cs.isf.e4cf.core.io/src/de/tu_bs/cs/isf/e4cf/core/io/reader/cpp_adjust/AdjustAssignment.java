9d5dba3e7cb9d155878684e38664f4478527c7e6
David Bumm <uusiy@student.kit.edu>
2022-12-04 10:06:18 +0100

    removed unused code

--- de.tu_bs.cs.isf.e4cf.core.io/src/de/tu_bs/cs/isf/e4cf/core/io/reader/cpp_adjust/AdjustAssignment.java
+++ de.tu_bs.cs.isf.e4cf.core.io/src/de/tu_bs/cs/isf/e4cf/core/io/reader/cpp_adjust/AdjustAssignment.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.impl.AttributeImpl;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.impl.NodeImpl;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.impl.StringValueImpl;
-import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.Attribute;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.Node;
 
 public class AdjustAssignment extends TreeAdjuster {
@@ -17,7 +16,6 @@ public class AdjustAssignment extends TreeAdjuster {
 		if (nodeType.equals(Const.EXPR_STMT) && !node.getChildren().isEmpty()) {
 			Node exprNode = node.getChildren().get(0);
 			if (!exprNode.getChildren().isEmpty()) {
-				String operator = Const.EMPTY;
 				boolean found = false;
 				boolean beforeOP = true;
 				List<Node> targetList = new ArrayList<>();
@@ -25,7 +23,6 @@ public class AdjustAssignment extends TreeAdjuster {
 				
 				for (Node child : exprNode.getChildren()) {
 					if (child.getNodeType().equals(Const.OPERATOR_SMALL) && child.getValueAt(0).equals(Const.EQ)) {
-						operator = child.getValueAt(0);
 						found = true;
 						beforeOP = false;
 					} 
@@ -39,12 +36,6 @@ public class AdjustAssignment extends TreeAdjuster {
 					Node assignment = new NodeImpl(Const.ASSIGNMENT);
 					assignment.setParent(node.getParent());
 					node.getParent().addChild(assignment,node.cut());
-					String target = Const.EMPTY;
-					for (Node n: targetList) {
-						if (!n.getAttributes().isEmpty()) {
-							target += n.getValueAt(0);
-						}
-					}
 					assignment.addAttribute(new AttributeImpl(Const.TARGET, new StringValueImpl(combineToString(targetList))));
 					assignment.addAttribute(new AttributeImpl(Const.OPERATOR_BIG, new StringValueImpl(Const.ASSIGN)));
 					
