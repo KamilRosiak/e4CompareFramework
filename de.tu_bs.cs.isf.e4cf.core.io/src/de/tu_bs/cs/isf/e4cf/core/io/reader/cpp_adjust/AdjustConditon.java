cb29880921c98e2a6d00b015bcde88fb77dd259a
David Bumm <uusiy@student.kit.edu>
2022-12-14 10:43:18 +0100

    basically removed ConditionAdjuster

--- de.tu_bs.cs.isf.e4cf.core.io/src/de/tu_bs/cs/isf/e4cf/core/io/reader/cpp_adjust/AdjustConditon.java
+++ de.tu_bs.cs.isf.e4cf.core.io/src/de/tu_bs/cs/isf/e4cf/core/io/reader/cpp_adjust/AdjustConditon.java
@@ -24,27 +24,7 @@ public class AdjustConditon extends TreeAdjuster {
 	@Override
 	protected void adjust(Node node, Node parent, String nodeType) {
 		if (nodeType.equals(Const.EXPR) && parent.getNodeType().equals(Const.CONDITION_BIG)) {
-			node.setNodeType(Const.BINARY_EXPR);
-			List<Node> children = node.getChildren();
-			Node child = null;
-			for (int i = 0; i < children.size(); i++) {
-				if (children.get(i).getNodeType().equals(Const.BINARY_EXPR)
-						|| children.get(i).getNodeType().equals(Const.OPERATOR_SMALL)) {
-					child = children.get(i);
-				}
-			}
-			if (child == null) {
-				node.cutWithoutChildren(); // no expression e.g while(true)
-				return;
-			}
-			for (Attribute a : node.getAttributes()) {
-				Node newNode = new NodeImpl(Const.NAME_EXPR, node);
-				newNode.addAttribute(a);
-			}
-			node.setAttributes(new ArrayList<>());
-			String operator = getOperatorString(child.getValueAt(0));
-			node.addAttribute(new AttributeImpl(Const.OPERATOR_BIG, new StringValueImpl(operator)));
-			child.cutWithoutChildren();
+			node.cutWithoutChildren();
 		}
 	}
 }
