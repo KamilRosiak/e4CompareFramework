ad8dc193d1a87bd00a369599ae425b6d2231ba05
Lukas Cronauer <42716375+lukcron@users.noreply.github.com>
2023-01-25 13:44:49 +0100

    Initial antlr grammar for .arch files
    Co-authored-by: Neduil <e.schmitz@tu-bs.de>

--- de.tu_bs.cs.isf.e4cf.compare.data_structures/src/de/tu_bs/cs/isf/e4cf/compare/data_structures/io/reader/arch/ArchReader.java
+++ de.tu_bs.cs.isf.e4cf.compare.data_structures/src/de/tu_bs/cs/isf/e4cf/compare/data_structures/io/reader/arch/ArchReader.java
@@ -1,14 +1,20 @@
 package de.tu_bs.cs.isf.e4cf.compare.data_structures.io.reader.arch;
 
-import org.eclipse.emf.ecore.EObject;
+import java.io.IOException;
+
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.enums.NodeType;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.impl.NodeImpl;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.AbstractArtifactReader;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.Node;
 import de.tu_bs.cs.isf.e4cf.compare.data_structures.interfaces.Tree;
+import de.tu_bs.cs.isf.e4cf.compare.data_structures.io.reader.arch.ArchLexer;
+import de.tu_bs.cs.isf.e4cf.compare.data_structures.io.reader.arch.ArchParser;
 import de.tu_bs.cs.isf.e4cf.core.file_structure.FileTreeElement;
-import de.tu_bs.cs.isf.familymining.ppu_iec.rcp_e4.EMFModelLoader.impl.EMFModelLoader;
 
 public class ArchReader extends AbstractArtifactReader {
 	public final static String[] SUPPORTED_FILE_ENDINGS = { "arch" };
@@ -19,8 +25,19 @@ public class ArchReader extends AbstractArtifactReader {
 
 	@Override
 	public Tree readArtifact(FileTreeElement fileTreeElement) {
-		EObject model = EMFModelLoader.load(fileTreeElement.getAbsolutePath(), "arch");
-		Node rootNode = new NodeImpl(NodeType.MODEL);
+		CharStream archFileStream;
+		try {
+			archFileStream = new ANTLRFileStream(fileTreeElement.getAbsolutePath());
+			ArchLexer archLexer = new ArchLexer(archFileStream);
+			CommonTokenStream commonTokenStream = new CommonTokenStream(archLexer);
+			ArchParser archParser = new ArchParser(commonTokenStream);
+			ArchParser.XmlContext xml = archParser.xml();
+			
+			ParseTreeWalker.DEFAULT.walk(null, xml);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
